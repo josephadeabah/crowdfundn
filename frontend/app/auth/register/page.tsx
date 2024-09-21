9c6b85ac4fc72cd76395bd5be767f298b4918d2e
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-09-21 13:33:46 +0000

    fix: add styling to customize overflow

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -184,7 +184,7 @@ export default function Register() {
     {
       label: 'Select Fundraising Category',
       content: (
-        <div className="overflow-y-auto max-h-[calc(100vh-400px)] h-full md:overflow-y-visible">
+        <div className="overflow-y-auto max-h-[calc(100vh-380px)] h-full [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-2 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
           <div className="w-full flex flex-wrap gap-2 mb-8 justify-start">
             {data.categories.map((category) => (
               <Badge
