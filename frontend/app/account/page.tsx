a00b6e617e97718bc4ae201e09ac8ff6a3869b43
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-09-25 12:45:59 +0000

    feat: add icons to the tabs

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
+import {
+  DashboardIcon,
+  HandIcon,
+  SymbolIcon,
+  ArchiveIcon,
+  IconJarLogoIcon,
+  RocketIcon,
+} from '@radix-ui/react-icons';
 import Rewards from './Rewards';
 import Campaigns from './Campaigns';
 import Transfers from './Transfers';
@@ -45,39 +53,40 @@ const ProfileTabs = () => {
       {/* Tabs Menu */}
       <div className="w-full md:w-[13%] border-b md:border-b-0 md:border-r-2 border-dashed border-red-200 dark:border-neutral-700">
         <nav
-          className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-1"
+          className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible  [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-1"
           aria-label="Tabs"
           role="tablist"
         >
           {[
-            'Dashboard',
-            'Donations',
-            'Transfers',
-            'Rewards',
-            'Campaigns',
-            'Archive',
-          ].map((tab) => (
+            { label: 'Dashboard', icon: <DashboardIcon /> },
+            { label: 'Donations', icon: <HandIcon /> },
+            { label: 'Transfers', icon: <SymbolIcon /> },
+            { label: 'Rewards', icon: <IconJarLogoIcon /> },
+            { label: 'Campaigns', icon: <RocketIcon /> },
+            { label: 'Archive', icon: <ArchiveIcon /> },
+          ].map(({ label, icon }) => (
             <button
-              key={tab}
+              key={label}
               type="button"
               className={`py-2 px-1 whitespace-nowrap text-sm font-medium md:text-base ${
-                activeTab === tab
+                activeTab === label
                   ? 'border-b-2 border-2 border-dashed md:border-b-0 md:border-l-4 md:border-r-0 border-red-200 text-red-600 dark:text-red-600'
                   : 'border-transparent text-gray-500 hover:bg-gray-100 dark:hover:bg-neutral-700 hover:text-red-600 dark:text-neutral-400 dark:hover:text-red-500'
               } flex items-center focus:outline-none`}
-              onClick={() => setActiveTab(tab)}
-              aria-selected={activeTab === tab}
-              aria-controls={`vertical-tab-${tab}`}
+              onClick={() => setActiveTab(label)}
+              aria-selected={activeTab === label}
+              aria-controls={`vertical-tab-${label}`}
               role="tab"
             >
-              {tab}
+              <span className="mr-2">{icon}</span>
+              {label}
             </button>
           ))}
         </nav>
       </div>
 
       {/* Tab Content */}
-      <div className="w-full bg-white dark:bg-gray-900 px-4 overflow-auto flex-1 [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-2 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
+      <div className="w-full bg-white dark:bg-gray-900 px-4 overflow-auto flex-1  [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-2 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
         <div
           role="tabpanel"
           id={`vertical-tab-${activeTab}`}
