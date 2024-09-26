2628ed6884b6dc5b0650775ab2bf07b778c4a5d4
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-09-26 07:40:08 +0000

    fix: mobile tab content

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -16,7 +16,7 @@ import Archive from './Archive';
 import Dashboard from './Dashboard';
 
 const ProfileTabs = () => {
-  const [activeTab, setActiveTab] = useState('');
+  const [activeTab, setActiveTab] = useState(''); // Set default to Dashboard
 
   useEffect(() => {
     const savedTab = localStorage.getItem('activeTab');
@@ -51,37 +51,19 @@ const ProfileTabs = () => {
   return (
     <div className="max-w-7xl mx-auto flex flex-col md:flex-row h-screen">
       {/* Tabs Menu */}
-      <div className="w-full md:w-[13%] border-b h-full md:border-b-0 md:border-r-2 border-dashed border-red-200 dark:border-neutral-700">
+      <div className="w-full md:w-[13%] border-b h-auto md:h-screen md:border-b-0 md:border-r-2 border-dashed border-red-200 dark:border-neutral-700">
         <nav
-          className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible  [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-1"
+          className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible"
           aria-label="Tabs"
           role="tablist"
         >
           {[
-            {
-              label: 'Dashboard',
-              icon: <DashboardIcon />,
-            },
-            {
-              label: 'Donations',
-              icon: <HandIcon />,
-            },
-            {
-              label: 'Transfers',
-              icon: <SymbolIcon />,
-            },
-            {
-              label: 'Rewards',
-              icon: <IconJarLogoIcon />,
-            },
-            {
-              label: 'Campaigns',
-              icon: <RocketIcon />,
-            },
-            {
-              label: 'Archive',
-              icon: <ArchiveIcon />,
-            },
+            { label: 'Dashboard', icon: <DashboardIcon /> },
+            { label: 'Donations', icon: <HandIcon /> },
+            { label: 'Transfers', icon: <SymbolIcon /> },
+            { label: 'Rewards', icon: <IconJarLogoIcon /> },
+            { label: 'Campaigns', icon: <RocketIcon /> },
+            { label: 'Archive', icon: <ArchiveIcon /> },
           ].map(({ label, icon }) => (
             <button
               key={label}
@@ -104,7 +86,7 @@ const ProfileTabs = () => {
       </div>
 
       {/* Tab Content */}
-      <div className="w-full bg-white dark:bg-gray-900 px-4 overflow-auto flex-1  [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-2 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
+      <div className="w-full bg-white dark:bg-gray-900 px-4 overflow-auto flex-1 h-full md:h-screen [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
         <div
           role="tabpanel"
           id={`vertical-tab-${activeTab}`}
