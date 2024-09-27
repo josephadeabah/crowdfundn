559fab024353c95b3f819178ff63a7b77b32495f
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-09-27 14:57:20 +0000

    fix: make description accepts richtext

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :full_name, :phone_number, :country, :payment_method, :currency, :birth_date, :category, :target_amount, :duration_in_days, :national_id, presence: true
   has_one :profile, dependent: :destroy
+  has_many :campaigns, foreign_key: 'fundraiser_id', dependent: :destroy
   accepts_nested_attributes_for :profile
 end
