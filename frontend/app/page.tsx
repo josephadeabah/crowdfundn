37c9dd705f55346fc8938103b38730bafcfa2203
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-10-03 15:57:38 +0000

    chore: change bg image

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -130,7 +130,7 @@ const HomePage = () => {
             className="absolute inset-0 bg-cover bg-center"
             style={{
               backgroundImage:
-                "url('https://images.unsplash.com/photo-1626624340240-aadc087844fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                "url('https://images.unsplash.com/photo-1589556763333-ad818080f39e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
               filter: 'brightness(0.4)',
             }}
           />
