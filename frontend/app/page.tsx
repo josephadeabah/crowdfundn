ab4257ee80e014695359e6d19a50ca82a2f070b8
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-10-03 15:09:04 +0000

    chore: change background image for hero

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -130,7 +130,7 @@ const HomePage = () => {
             className="absolute inset-0 bg-cover bg-center"
             style={{
               backgroundImage:
-                "url('https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')",
+                "url('https://images.unsplash.com/photo-1626624340240-aadc087844fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
               filter: 'brightness(0.4)',
             }}
           />
