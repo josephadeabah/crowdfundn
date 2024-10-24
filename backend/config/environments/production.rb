16d8460792fbbd956f4978d9356c62c097583ed7
josephadeabah <abeansah.lx@gmail.com>
2024-10-24 22:44:25 +0000

    feat: store images in digital ocean spaces

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -32,12 +32,14 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  config.action_controller.asset_host = ENV['DO_SPACES_ENDPOINT']
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :digital_ocean
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
