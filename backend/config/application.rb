c30c2e2f08a4f139bcda42ac1a993a0a4db9027f
josephadeabah <abeansah.lx@gmail.com>
2024-10-27 14:32:00 +0000

    fix: add cors to config/application.rb

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,15 +11,24 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # Autoload lib settings
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # CORS configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://crowdfundn.vercel.app',            # Replace with your production Next.js URL
+                'http://localhost:3000',                         # Local development for Next.js
+                'https://bantuhive-api-dicht.ondigitalocean.app' # Rails API on DigitalOcean
+
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                 expose: ['Authorization']
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
