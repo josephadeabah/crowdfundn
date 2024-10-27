e8ce7e60bf5bdd73a4329eff1ee81fd244eed2fa
josephadeabah <abeansah.lx@gmail.com>
2024-10-27 08:29:58 +0000

    fix: enable asset url in cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://crowdfundn.vercel.app'
+    origins 'http://localhost:3000', 'https://crowdfundn.vercel.app', 'https://bantuhive-api-dicht.ondigitalocean.app'
 
     resource '*',
              headers: :any,
