2bfe87bd7c7533de19746c56860b2d51cde467c7
josephadeabah <abeansah.lx@gmail.com>
2024-10-31 20:09:11 +0000

    fix: add dark mode classes to empty and error pages

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -33,11 +33,6 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
   if (loading) return <CampaignCardLoader />;
   if (error) return <ErrorPage />;
 
-  // Check if campaigns array is empty
-  if (campaigns.length === 0) {
-    return <EmptyPage />;
-  }
-
   // Memoize the shuffled campaigns to avoid re-shuffling on each render
   const shuffledCampaigns = useMemo(() => shuffleArray(campaigns), [campaigns]);
 
@@ -45,41 +40,47 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
   const displayedCampaigns = shuffledCampaigns.slice(0, 6);
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-2 md:p-0">
-      {displayedCampaigns.map((campaign, index) => (
-        <motion.div
-          key={campaign.id}
-          initial="hidden"
-          animate="visible"
-          transition={{ duration: 0.5, delay: index * 0.1 }}
-          className="bg-white flex flex-col h-full dark:bg-gray-800 dark:text-gray-50 transform hover:scale-105 transition-transform duration-300 cursor-pointer"
-        >
-          <Link href={`/campaign/${campaign.id}?${generateRandomString()}`}>
-            <div className="flex flex-col h-full dark:bg-gray-800 dark:text-gray-50 transform hover:scale-105 transition-transform duration-300 cursor-pointer">
-              <img
-                src={campaign?.media}
-                alt="media thumbnail"
-                className="mb-2 object-cover h-32 w-full"
-              />
-              <div className="px-1">
-                <h3 className="text-lg font-bold truncate whitespace-nowrap overflow-hidden">
-                  {campaign?.title}
-                </h3>
-                <div className="w-full text-xs">
-                  <Progress
-                    firstProgress={33}
-                    firstTooltipContent={`Performance: ${33}%`}
+    <div>
+      {campaigns.length === 0 ? (
+        <EmptyPage />
+      ) : (
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-2 md:p-0">
+          {displayedCampaigns.map((campaign, index) => (
+            <motion.div
+              key={campaign.id}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              className="bg-white flex flex-col h-full dark:bg-gray-800 dark:text-gray-50 transform hover:scale-105 transition-transform duration-300 cursor-pointer"
+            >
+              <Link href={`/campaign/${campaign.id}?${generateRandomString()}`}>
+                <div className="flex flex-col h-full dark:bg-gray-800 dark:text-gray-50 transform hover:scale-105 transition-transform duration-300 cursor-pointer">
+                  <img
+                    src={campaign?.media}
+                    alt="media thumbnail"
+                    className="mb-2 object-cover h-32 w-full"
                   />
+                  <div className="px-1">
+                    <h3 className="text-lg font-bold truncate whitespace-nowrap overflow-hidden">
+                      {campaign?.title}
+                    </h3>
+                    <div className="w-full text-xs">
+                      <Progress
+                        firstProgress={33}
+                        firstTooltipContent={`Performance: ${33}%`}
+                      />
+                    </div>
+                    <p className="flex justify-between items-center text-sm font-semibold mt-2">
+                      {campaign?.current_amount}{' '}
+                      <span className="font-normal">raised</span>
+                    </p>
+                  </div>
                 </div>
-                <p className="flex justify-between items-center text-sm font-semibold mt-2">
-                  {campaign?.current_amount}{' '}
-                  <span className="font-normal">raised</span>
-                </p>
-              </div>
-            </div>
-          </Link>
-        </motion.div>
-      ))}
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
