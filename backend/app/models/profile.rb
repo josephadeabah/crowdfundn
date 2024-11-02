a9150b2d594c68535f0a9b45cbc65a15d3888c69
josephadeabah <abeansah.lx@gmail.com>
2024-11-02 15:11:04 +0000

    fix: currency error

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -13,9 +13,9 @@ class Profile < ApplicationRecord
   validates :avatar, presence: true, allow_blank: true # Optional, depending on your requirements
 
   # Validations for new fields
-  # validates :currency, presence: true, allow_blank: true
-  validates :currency_code, presence: true
-  validates :currency_symbol, presence: true
+  # validates :currency, presence: true
+  # validates :currency_code, presence: true
+  # validates :currency_symbol, presence: true
 
   # Optional associations, depending on your app structure:
   # has_one_attached :avatar (if you're using Active Storage for file uploads)
