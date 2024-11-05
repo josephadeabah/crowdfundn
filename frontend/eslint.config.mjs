a2a4151273436b665a63ab0348f601b2512e12db
josephadeabah <abeansah.lx@gmail.com>
2024-11-05 13:22:23 +0000

    fix: disable eslint unused vars

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,4 +5,9 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['off'], // Disable the rule for unused vars
+    },
+  }
 );
