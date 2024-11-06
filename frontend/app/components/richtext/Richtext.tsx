954098106c378f993aa867b7fb24fe8e01f6d211
josephadeabah <abeansah.lx@gmail.com>
2024-11-06 16:53:34 +0000

    fix: downgrade next

--- frontend/app/components/richtext/Richtext.tsx
+++ frontend/app/components/richtext/Richtext.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 
 const RichTextEditor = dynamic(() => import('@mantine/rte'), {
   ssr: false,
-  loading: () => <div>Loading editor...</div>,
+  loading: () => null,
 });
 
 export default RichTextEditor;
