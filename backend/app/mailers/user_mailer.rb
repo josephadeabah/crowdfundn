b0e41f0e87a5ca479e72347bda320cec0ef3caba
josephadeabah <abeansah.lx@gmail.com>
2024-11-16 21:33:48 +0000

    fix: add our own email delivery service with active mailer and job

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@yourdomain.com'
+    default from: 'no-reply@bantuhive-api-dicht.ondigitalocean.app'
   
     def charge_success_email(user, donation)
       @user = user
