ed764e5f3d5435934b3179bbc934b9552fb85a66
josephadeabah <abeansah.lx@gmail.com>
2024-11-16 00:06:49 +0000

    feat: add subscription

--- backend/app/controllers/api/v1/fundraisers/donations_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/donations_controller.rb
@@ -65,7 +65,7 @@ module Api
 
           # Prepare metadata for Paystack initialization
           metadata = { 
-            user_id: @current_user&.id, 
+            user_id: donation.donation.user_id, 
             campaign_id: donation.campaign_id,
             session_token: donation.metadata[:session_token] # Only for anonymous users
           }
