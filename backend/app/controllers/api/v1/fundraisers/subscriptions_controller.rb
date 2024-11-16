6814fec04899177e57beb287ca616ab9fb292c75
josephadeabah <abeansah.lx@gmail.com>
2024-11-16 09:31:19 +0000

    fix: name for subscription

--- backend/app/controllers/api/v1/fundraisers/subscriptions_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/subscriptions_controller.rb
@@ -7,10 +7,16 @@ module Api
     
           def create_plan
             paystack_service = PaystackService.new
+             # Fetch the campaign by the campaign_id from the parameters
+            campaign = Campaign.find(params[:campaign_id])
+
+            # Fetch the title of the campaign to use as the subscription name
+            campaign_title = campaign.title
+
             response = paystack_service.create_subscription_plan(
-              name: params[:name],
+              name: campaign_title, 
               interval: params[:interval],
-              amount: params[:amount] * 100, # Convert to kobo
+              amount: params[:amount],
             )
     
             if response[:status]
