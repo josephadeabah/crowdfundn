afe1f3b08e36123c5f140a7a8e5e38360474c462
josephadeabah <abeansah.lx@gmail.com>
2024-11-19 10:38:40 +0000

    fix: add error logs for mailer

--- backend/app/jobs/donation_email_job.rb
+++ backend/app/jobs/donation_email_job.rb
@@ -1,8 +1,9 @@
 class DonationEmailJob < ApplicationJob
   queue_as :mailers
 
-  def perform(donation_id)
-    donation = Donation.find(donation_id)
+  def perform(donation)
+    Rails.logger.info "Starting DonationEmailJob for donation ID: #{donation.id}"
     UserMailer.donation_success_email(donation).deliver_now
+    Rails.logger.info "DonationEmailJob completed for donation ID: #{donation.id}"
   end
 end
