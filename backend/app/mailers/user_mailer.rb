74be03429fd464bd8fff33dc4ab9d1f6ee2e5a60
josephadeabah <abeansah.lx@gmail.com>
2024-11-19 11:40:26 +0000

    fix: change default email

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@bantuhive.com'
+    default from: 'help@bantuhive.com'
   
     def donation_success_email(donation)
       @donation = donation
