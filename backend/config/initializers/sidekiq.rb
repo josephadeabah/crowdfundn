f18eb8b143bfea9141e793d332fd38b4d4db663f
josephadeabah <abeansah.lx@gmail.com>
2024-11-19 09:26:20 +0000

    feat: add sidekiq job with mailer

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 Sidekiq.configure_server do |config|
     config.redis = {
-      url: ENV.fetch("UPSTASH_REDIS_REST_URL"),
-      password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN"),
+      url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
+      password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
       size: 5 # Limit connection pool size
     }
   end
   
-Sidekiq.configure_client do |config|
+  Sidekiq.configure_client do |config|
     config.redis = {
-      url: ENV.fetch("UPSTASH_REDIS_REST_URL"),
-      password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN"),
+      url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
+      password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
       size: 1 # Reduce client pool size
     }
   end
