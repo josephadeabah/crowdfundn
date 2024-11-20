a90c0970e296889ab9be83614c420cf700473ad5
josephadeabah <abeansah.lx@gmail.com>
2024-11-20 04:38:02 +0000

    fix: cors for headers

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://bantuhive.com', 'https://www.bantuhive.com', 'https://bantuhive-api-dicht.ondigitalocean.app'
+    origins 'http://localhost:3000', 'https://bantuhive.com', 'https://www.bantuhive.com'
 
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
-             expose: ['Authorization'],
-             credentials: true
+             credentials: true,
+             expose: ['Authorization']
   end
 end
