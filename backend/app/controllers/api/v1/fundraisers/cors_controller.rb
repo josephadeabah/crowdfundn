9005662a607ee31747e18f3cba4639ca224198e8
josephadeabah <abeansah.lx@gmail.com>
2024-11-20 13:59:54 +0000

    fix: manually add cors to routes

--- backend/app/controllers/api/v1/fundraisers/cors_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/cors_controller.rb
@@ -10,11 +10,27 @@ class CorsController < ApplicationController
     protected
 
     def cors_set_access_control_headers
-        response.headers['Access-Control-Allow-Origin'] = "https://www.bantuhive.com"
+        response.headers['Access-Control-Allow-Origin'] = check_origin
         response.headers['Access-Control-Allow-Credentials'] = "true"
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
         response.headers['Access-Control-Max-Age'] = '86400'
+    end
+
+    def check_origin
+        permitted_origins = Set[
+            "https://bantuhive.com",
+            "http://127.0.0.1:3000",
+            "https://www.bantuhive.com",
+        ]
+
+        origin = request.origin
+
+        if permitted_origins.include?(origin)
+            origin
+        else
+            render json: { error: "Origin not permitted" }
+        end
     end
 
 end
