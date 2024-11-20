4717d288e297d7d2fba29dc4a73ba275b11439fb
josephadeabah <abeansah.lx@gmail.com>
2024-11-20 12:11:16 +0000

    fix: include specific headers

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000', 'https://www.bantuhive.com', 'https://bantuhive.com'
 
     resource '*',
-             headers: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'X-Prototype-Version', 'X-CSRF-Token', 'X-CSRF-Param', 'X-User-Email', 'X-User-Token'],
+             headers: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'X-Prototype-Version', 'X-CSRF-Token', 'X-CSRF-Param', 'X-User-Email', 'X-User-Token', 'Origin', 'application/json'],
              methods: %i[get post put patch delete],
              credentials: true,
              max_age: 86400
