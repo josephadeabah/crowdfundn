fff26b1295056acea2e81fcb53767b3a6a43e739
josephadeabah <abeansah.lx@gmail.com>
2024-11-23 06:29:29 +0000

    feat: add more handlers for  webhook

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,16 +1,15 @@
-# Sidekiq.configure_server do |config|
-#     config.redis = {
-#       url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
-#       password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
-#       size: 10 # Limit connection pool size
-#     }
-#   end
+Sidekiq.configure_server do |config|
+    config.redis = {
+      url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
+      password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
+      size: 10 # Limit connection pool size
+    }
+  end
   
-#   Sidekiq.configure_client do |config|
-#     config.redis = {
-#       url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
-#       password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
-#       size: 1 # Reduce client pool size
-#     }
-#   end
-  
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV.fetch("UPSTASH_REDIS_REST_URL").strip,
+    password: ENV.fetch("UPSTASH_REDIS_REST_TOKEN").strip,
+    size: 1 # Reduce client pool size
+  }
+end
