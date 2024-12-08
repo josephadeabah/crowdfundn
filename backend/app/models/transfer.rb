1c9df8dbf9765a1c417f16b0069f2edfac625990
josephadeabah <abeansah.lx@gmail.com>
2024-12-08 04:41:01 +0000

    chore: make optional column for campaign

--- backend/app/models/transfer.rb
+++ backend/app/models/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer < ApplicationRecord
   belongs_to :user
-  belongs_to :campaign
+  belongs_to :campaign, optional: true
 
   validates :recipient_code, presence: true
   validates :bank_name, :account_number, :amount, :transfer_code, :reference, presence: true
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_07_224907) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_08_043921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -215,7 +215,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_07_224907) do
 
   create_table "transfers", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "campaign_id", null: false
+    t.bigint "campaign_id"
     t.string "bank_name"
     t.string "account_number"
     t.decimal "amount"
