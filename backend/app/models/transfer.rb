a8c8aaa448826fb369bdef21c123d201ef131c3a
josephadeabah <abeansah.lx@gmail.com>
2024-12-08 02:49:52 +0000

    chore: simplified the data structure returned

--- backend/app/models/transfer.rb
+++ backend/app/models/transfer.rb
@@ -3,5 +3,5 @@ class Transfer < ApplicationRecord
   belongs_to :campaign
 
   validates :recipient_code, presence: true
-  validates :business_name, :account_number, :amount, :transfer_code, :reference, presence: true
+  validates :bank_name, :account_number, :amount, :transfer_code, :reference, presence: true
 end
