f9b71020e16a2f24764f9db66beeb242bd02a9a2
josephadeabah <abeansah.lx@gmail.com>
2024-12-10 12:46:03 +0000

    feat: add pagination

--- frontend/app/account/Transfers.tsx
+++ frontend/app/account/Transfers.tsx
@@ -194,36 +194,38 @@ export default function Transfers() {
           <h3 className="text-lg font-semibold text-gray-800 dark:text-white mb-4">
             Transaction History
           </h3>
-          <div className="overflow-x-auto [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
+          <div className="overflow-x-auto [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
             <table className="min-w-full bg-white dark:bg-neutral-800 rounded-lg">
-              <thead className="bg-gray-50 dark:bg-neutral-700">
-                <tr>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Amount
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Date
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Status
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Reference
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Account Number
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Settlement Bank
-                  </th>
-                  <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-                    Reason
-                  </th>
-                </tr>
-              </thead>
+              {!loading && (
+                <thead className="bg-gray-50 dark:bg-neutral-700">
+                  <tr>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Amount
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Date
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Status
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Reference
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Account Number
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Settlement Bank
+                    </th>
+                    <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
+                      Reason
+                    </th>
+                  </tr>
+                </thead>
+              )}
               <tbody>
                 {loading ? (
-                  <TransferLoader />
+                  <TransferLoader /> // Loader only for the body (without header)
                 ) : transfers?.length === 0 ? (
                   <tr>
                     <td
@@ -268,11 +270,11 @@ export default function Transfers() {
           </div>
           {/* Pagination Component */}
           <div className="w-full max-w-3xl mx-auto py-12">
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={fetchTransfers}
-          />
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={fetchTransfers}
+            />
           </div>
         </div>
       </div>
--- frontend/app/loaders/TransferLoader .tsx
+++ frontend/app/loaders/TransferLoader .tsx
@@ -4,62 +4,35 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const TransferLoader = () => {
   return (
-    <table>
-      <thead className="bg-gray-50 dark:bg-neutral-700">
-        <tr>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Amount
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Date
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Status
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Reference
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Account Number
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Settlement Bank
-          </th>
-          <th className="px-4 py-2 text-left text-gray-600 dark:text-neutral-300 whitespace-nowrap">
-            Reason
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array(8) // Assuming there are 5 rows of transfers, adjust as necessary
-          .fill(null)
-          .map((_, index) => (
-            <tr key={index}>
-              <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
-                <Skeleton width={100} />
-              </td>
-              <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
-                <Skeleton width={120} />
-              </td>
-              <td className="px-4 py-2 text-green-500 dark:text-green-400 whitespace-nowrap">
-                <Skeleton width={80} />
-              </td>
-              <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
-                <Skeleton width={150} />
-              </td>
-              <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
-                <Skeleton width={120} />
-              </td>
-              <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
-                <Skeleton width={150} />
-              </td>
-              <td className="px-4 py-2 truncate text-gray-800 dark:text-white whitespace-nowrap overflow-hidden text-ellipsis">
-                <Skeleton width={200} />
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <tbody>
+      {Array(8) // Assuming there are 8 rows to be loaded
+        .fill(null)
+        .map((_, index) => (
+          <tr key={index}>
+            <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
+              <Skeleton width={100} />
+            </td>
+            <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
+              <Skeleton width={120} />
+            </td>
+            <td className="px-4 py-2 text-green-500 dark:text-green-400 whitespace-nowrap">
+              <Skeleton width={80} />
+            </td>
+            <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
+              <Skeleton width={150} />
+            </td>
+            <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
+              <Skeleton width={120} />
+            </td>
+            <td className="px-4 py-2 text-gray-800 dark:text-white whitespace-nowrap">
+              <Skeleton width={150} />
+            </td>
+            <td className="px-4 py-2 truncate text-gray-800 dark:text-white whitespace-nowrap overflow-hidden text-ellipsis">
+              <Skeleton width={200} />
+            </td>
+          </tr>
+        ))}
+    </tbody>
   );
 };
 
