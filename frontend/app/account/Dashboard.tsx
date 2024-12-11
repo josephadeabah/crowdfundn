0453a4dff70cb96fecb003340af606c94d7d2d54
josephadeabah <abeansah.lx@gmail.com>
2024-12-11 10:39:18 +0000

    fix: percenttage progress with transfer amount

--- frontend/app/account/Dashboard.tsx
+++ frontend/app/account/Dashboard.tsx
@@ -152,7 +152,7 @@ export default function Dashboard() {
         {/* Boost Campaign Card */}
         <Card className="p-4 bg-white dark:bg-neutral-800 rounded-lg border-none shadow hover:bg-gray-100 transition-shadow duration-200">
           <CardHeader>
-            <CardTitle className="text-lg font-semibold text-red-600 dark:text-gray-400">
+            <CardTitle className="flex gap-2 text-lg font-semibold text-red-600 dark:text-gray-400">
               <InfoCircledIcon/>
               Attention
             </CardTitle>
