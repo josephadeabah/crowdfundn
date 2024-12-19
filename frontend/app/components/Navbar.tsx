baab4ca895b8da5a8f27ff09814fda73b79e35f6
josephadeabah <abeansah.lx@gmail.com>
2024-12-19 22:25:21 +0000

    fix: remove chart.js

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -235,11 +235,11 @@ const Navbar = () => {
                         <span className="font-semibold">{user.full_name}</span>
                         <span className="text-gray-600">{user.email}</span>
                       </div>
-                      <div className="flex items-center gap-3 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 px-0 py-2 rounded transition">
+                      {/* <div className="flex items-center gap-3 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 px-0 py-2 rounded transition">
                         <Link href="/account">
                           <div>Go to Account</div>
                         </Link>
-                      </div>
+                      </div> */}
                       <div
                         className="hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 p-2 rounded transition"
                         onClick={logout}
@@ -301,11 +301,11 @@ const Navbar = () => {
                           <span className="text-gray-600">{user.email}</span>
                         </div>
                       </div>
-                      {/* <div className="flex items-center gap-3 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 px-0 py-2 rounded transition">
+                      <div className="flex items-center gap-3 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 px-0 py-2 rounded transition">
                         <Link href="/account">
                           <div>Go to Account</div>
                         </Link>
-                      </div> */}
+                      </div>
                       <div className="flex items-center gap-3 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-700 px-0 py-2 rounded transition">
                         <DarkModeBtn />
                         <div>Change Theme</div>
