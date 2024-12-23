ea78365f64f02dc1bd6ff46bce0d81a8d3a2003b
josephadeabah <abeansah.lx@gmail.com>
2024-12-23 11:17:05 +0000

    feat: add content for marketing

--- frontend/app/articles/how-to-get-started/page.tsx
+++ frontend/app/articles/how-to-get-started/page.tsx
@@ -5,17 +5,17 @@ import html from 'remark-html';
 import { Button } from '@/app/components/button/Button';
 
 export default async function HowToGetStarted() {
-  // Path to the markdown file
-  const markdownFilePath = path.join(process.cwd(), 'content', 'how-to-get-started.md');
+    // Path to the markdown file
+    const markdownFilePath = path.join(process.cwd(), 'public', 'content', 'how-to-get-started.md');
   
-  // Read the markdown file
-  const fileContent = fs.readFileSync(markdownFilePath, 'utf-8');
-  
-  // Parse the markdown content to HTML using remark
-  const processedContent = await remark().use(html).process(fileContent);
-  
-  // Convert buffer to string
-  const content = processedContent.toString();
+    // Read the markdown file
+    const fileContent = fs.readFileSync(markdownFilePath, 'utf-8');
+
+    // Parse the markdown content to HTML using remark
+    const processedContent = await remark().use(html).process(fileContent);
+
+    // Convert buffer to string
+    const content = processedContent.toString();
 
   return (
     <div className="relative w-full max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
