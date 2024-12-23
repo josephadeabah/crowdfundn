8f437f24822ba990f3e9e5cb82e9172852210fdc
josephadeabah <abeansah.lx@gmail.com>
2024-12-23 11:09:11 +0000

    feat: add content for marketing

--- frontend/app/articles/how-to-get-started/page.tsx
+++ frontend/app/articles/how-to-get-started/page.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
 import fs from 'fs';
 import path from 'path';
-import { GetStaticProps } from 'next';
 import { remark } from 'remark';
 import html from 'remark-html';
 import { Button } from '@/app/components/button/Button';
 
-// Component to render the Markdown content
-const HowToGetStarted = ({ content }: { content: string }) => {
+export default async function HowToGetStarted() {
+  // Path to the markdown file
+  const markdownFilePath = path.join(process.cwd(), 'content', 'how-to-get-started.md');
+  
+  // Read the markdown file
+  const fileContent = fs.readFileSync(markdownFilePath, 'utf-8');
+  
+  // Parse the markdown content to HTML using remark
+  const processedContent = await remark().use(html).process(fileContent);
+  
+  // Convert buffer to string
+  const content = processedContent.toString();
+
   return (
     <div className="relative w-full max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <h1 className="text-4xl font-bold text-center mb-8 text-green-800">
@@ -28,27 +37,4 @@ const HowToGetStarted = ({ content }: { content: string }) => {
       </div>
     </div>
   );
-};
-
-// Next.js function to read and parse the Markdown file during build time
-export const getStaticProps: GetStaticProps = async () => {
-  // Path to the markdown file
-  const markdownFilePath = path.join(process.cwd(), 'content', 'how-to-get-started.md');
-  
-  // Read the markdown file
-  const fileContent = fs.readFileSync(markdownFilePath, 'utf-8');
-  
-  // Parse the markdown content to HTML using remark
-  const processedContent = await remark().use(html).process(fileContent);
-  
-  // Convert buffer to string
-  const content = processedContent.toString();
-  
-  return {
-    props: {
-      content,
-    },
-  };
-};
-
-export default HowToGetStarted;
+}
