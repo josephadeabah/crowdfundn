b40a48695fd993bacff0917b1cd4581e4b757a29
josephadeabah <abeansah.lx@gmail.com>
2024-12-24 14:20:47 +0000

    fix: add sorting

--- frontend/app/explore/advance/page.tsx
+++ frontend/app/explore/advance/page.tsx
@@ -37,6 +37,7 @@ const CampaignsPage = () => {
 
   return (
     <div className="container mx-auto p-4">
+        <span className="text-sm font-normal mb-4 text-red-600">Work In Progress</span>
       <h1 className="text-2xl font-bold mb-4">We’ve made it easy to find the causes that matter most to you.</h1>
 
       <div className="flex flex-wrap gap-4 mb-6">
