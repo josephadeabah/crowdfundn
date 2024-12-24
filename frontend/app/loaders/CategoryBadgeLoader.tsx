3f091d7ccb18c9848763cb2173b242c48708904e
josephadeabah <abeansah.lx@gmail.com>
2024-12-24 21:01:26 +0000

    fix: breaks on donations page

--- frontend/app/loaders/CategoryBadgeLoader.tsx
+++ frontend/app/loaders/CategoryBadgeLoader.tsx
@@ -4,20 +4,16 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const CategoryBadgeLoader: React.FC = () => {
   return (
-    <div className="w-full px-2 py-4 bg-gradient-to-br from-gray-50 to-neutral-50 dark:from-gray-900 dark:to-gray-800">
-      <div className="max-w-7xl mx-auto">
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2">
-          {Array.from({ length: 24 }).map((_, index) => (
-              <Skeleton
-                key={index}
-                width={100}
-                height={30}
-                className="cursor-pointer transform hover:scale-105 transition-transform duration-300 w-fit"
-              />
-        ))}
-          </div>
-      </div>
-    </div>
+    <>
+      {Array.from({ length: 12 }).map((_, index) => (
+        <Skeleton
+          key={index}
+          width={100}
+          height={30}
+          className="cursor-pointer transform hover:scale-105 transition-transform duration-300 w-fit"
+        />
+      ))}
+    </>
   );
 };
 
