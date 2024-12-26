d9a6512143833c53ef727387705513e461a68cf7
josephadeabah <abeansah.lx@gmail.com>
2024-12-26 16:54:24 +0000

    fix: add fallback for no donations

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -294,7 +294,7 @@ const SingleCampaignPage: React.FC = () => {
 
         {selectedTab === 'backers' && (
           <div className="max-w-xl bg-white dark:bg-gray-800 mx-auto px-6 py-6">
-            <h3 className="text-2xl font-bold mb-6">Backer List</h3>
+            <h3 className="text-2xl font-bold mb-6">Backers</h3>
             <DonationList
               donations={donations}
               fundraiserCurrency={fundraiserCurrency}
