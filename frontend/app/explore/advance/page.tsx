83d3fb459ca4f998b62b73adc38ab8670550dbec
josephadeabah <abeansah.lx@gmail.com>
2024-12-26 20:48:07 +0000

    fix: revrt search button

--- frontend/app/explore/advance/page.tsx
+++ frontend/app/explore/advance/page.tsx
@@ -131,7 +131,7 @@ const CampaignsPage = () => {
           initial="hidden"
           animate={controls}
           transition={{ duration: 0.5 }}
-          className="text-4xl font-bold text-center text-gray-800 py-4"
+          className="text-4xl font-bold text-center text-gray-800 py-6"
         >
           Find and support the causes that matter most to
           you.
