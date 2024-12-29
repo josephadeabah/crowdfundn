a4defc514f127cf4f7035f891d92c106f05e5110
josephadeabah <abeansah.lx@gmail.com>
2024-12-29 16:55:47 +0000

    fix: remove the deleting of recipient code anytime update subaccount

--- backend/app/controllers/api/v1/members/users_controller.rb
+++ backend/app/controllers/api/v1/members/users_controller.rb
@@ -138,12 +138,9 @@ module Api
                   raise StandardError, "Failed to delete recipient on Paystack: #{delete_response[:message]}"
                 end
               end
-        
               # Delete recipient code locally
               user.subaccount.update!(recipient_code: nil)
-            end
-        
-          # Check if recipient_code exists. If it does not exist, create it only once.
+               # Check if recipient_code exists. If it does not exist, create it only once.
           if subaccount.recipient_code.blank?
             # Automatically create recipient if missing
             response = PaystackService.new.create_transfer_recipient(
@@ -175,6 +172,9 @@ module Api
               return
             end
           end
+            end
+        
+         
         
           # If recipient_code exists or has been created, proceed to update the subaccount
           response = PaystackService.new.update_subaccount(
