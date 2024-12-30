4dd845e9bb79217647d7398b258fa11dbe9e5779
josephadeabah <abeansah.lx@gmail.com>
2024-12-30 22:49:58 +0000

    fix: name for donor

--- backend/app/services/paystack_webhook/charge_success_handler.rb
+++ backend/app/services/paystack_webhook/charge_success_handler.rb
@@ -81,7 +81,7 @@ class PaystackWebhook::ChargeSuccessHandler
         amount: net_amount,
         user_id: user_id.presence,  # Update user_id only if provided
         campaign_id: campaign_id.presence,  # Update campaign_id only if provided
-        full_name: response.dig(:data, :metadata, :fundraiserName),
+        # full_name: response.dig(:data, :metadata, :fundraiserName),
         email: response.dig(:data, :customer, :email),
         phone: response.dig(:data, :customer, :phone),
         metadata: donation.metadata.merge(session_token: session_token)  # Add session_token to metadata
