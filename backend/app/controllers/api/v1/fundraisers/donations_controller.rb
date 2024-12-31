d479a6fbcde9bde743f2a3a0cd16919862c67393
josephadeabah <abeansah.lx@gmail.com>
2024-12-31 00:16:45 +0000

    fix: name for donor

--- backend/app/controllers/api/v1/fundraisers/donations_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/donations_controller.rb
@@ -111,7 +111,7 @@ module Api
           donation.phone = params[:donation][:phone]
         
           metadata = { 
-            user_id: @current_user.id, # ID of the authenticated donor
+            user_id: donation.user_id, # ID of the authenticated donor
             campaign_id: donation.campaign_id,
             session_token: donation.metadata[:session_token], # Only for anonymous users
             donor_name: donation.full_name,
