07c8f2f4c3355c21e64ea7f35a5caa1a47d56d64
josephadeabah <abeansah.lx@gmail.com>
2024-12-31 05:12:31 +0000

    fix: seperate campaign cards from explore

--- frontend/app/components/comments/CommentSection.tsx
+++ frontend/app/components/comments/CommentSection.tsx
@@ -114,7 +114,7 @@ const CommentsSection: React.FC<CommentsSectionProps> = ({ campaignId }) => {
       </div>
 
       {areCommentsVisible && (
-        <div className="max-h-96 overflow-y-auto scrollbar">
+        <div className="max-h-96 overflow-y-auto [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
           {fetchLoading ? (
             Array.from({ length: 3 }).map((_, index) => (
               <CommentLoader key={index} />
