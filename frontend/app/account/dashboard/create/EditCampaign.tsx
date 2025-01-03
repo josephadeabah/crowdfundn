ff54a3bb46c4a5e99b19859d4d83cb7978a892b3
josephadeabah <abeansah.lx@gmail.com>
2025-01-03 23:28:44 +0000

    fix: campaign settings

--- frontend/app/account/dashboard/create/EditCampaign.tsx
+++ frontend/app/account/dashboard/create/EditCampaign.tsx
@@ -231,7 +231,7 @@ const EditCampaign = () => {
             />
           </div>
         )}
-        <div className="w-full bg-white text-center mt-4 h-10 text-gray-100">
+        <div className="w-full bg-white text-center mt-4 h-10 text-gray-300">
           &copy; {new Date().getFullYear()} BantuHive Ltd. All rights reserved.
         </div>
       </div>
--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -188,7 +188,7 @@ const ProfileTabs = () => {
           />
         )}
       </div>
-      <div className="w-full bg-white text-center mt-4 h-10 text-gray-100">
+      <div className="w-full bg-white text-center mt-4 h-10 text-gray-300">
         &copy; {new Date().getFullYear()} BantuHive Ltd. All rights reserved.
       </div>
     </div>
