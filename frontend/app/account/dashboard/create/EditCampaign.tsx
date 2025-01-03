8cd777998ae37f677093e4ad971411e2684038d4
josephadeabah <abeansah.lx@gmail.com>
2025-01-03 23:33:34 +0000

    fix: campaign settings

--- frontend/app/account/dashboard/create/EditCampaign.tsx
+++ frontend/app/account/dashboard/create/EditCampaign.tsx
@@ -231,9 +231,6 @@ const EditCampaign = () => {
             />
           </div>
         )}
-        <div className="w-full bg-white text-center mt-4 h-10 text-gray-300">
-          &copy; {new Date().getFullYear()} BantuHive Ltd. All rights reserved.
-        </div>
       </div>
       {/* Modal Component */}
       <Modal
--- frontend/app/account/dashboard/create/page.tsx
+++ frontend/app/account/dashboard/create/page.tsx
@@ -4,6 +4,9 @@ const AddCampaign = () => {
   return (
     <div>
       <FundraiserPage />
+      <div className="w-full bg-white text-center mt-4 h-10 text-gray-300">
+          &copy; {new Date().getFullYear()} BantuHive Ltd. All rights reserved.
+        </div>
     </div>
   );
 };
