d198769719df8ba150e225524942e9e98f1d9d34
josephadeabah <abeansah.lx@gmail.com>
2025-01-03 23:43:33 +0000

    fix: campaign settings

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -188,7 +188,7 @@ const ProfileTabs = () => {
           />
         )}
       </div>
-      <div className="w-full bg-white text-center mt-4 h-10 text-gray-300">
+      <div className="flex justify-center items-center w-full bg-white mt-4 h-10 text-gray-300">
         &copy; {new Date().getFullYear()} BantuHive Ltd. All rights reserved.
       </div>
     </div>
