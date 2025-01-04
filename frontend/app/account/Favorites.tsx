78389c174b8ca0e1b7c18428d224083460e24ad8
josephadeabah <abeansah.lx@gmail.com>
2025-01-04 21:06:57 +0000

    fix: add favorited property to response json

--- frontend/app/account/Favorites.tsx
+++ frontend/app/account/Favorites.tsx
@@ -1,17 +1,22 @@
-'use client';
-
 import React, { useEffect } from 'react';
 import { useCampaignContext } from '@/app/context/account/campaign/CampaignsContext';
 import CampaignCardLoader from '@/app/loaders/CampaignCardLoader';
 import Image from 'next/image';
 import Link from 'next/link';
 import { generateRandomString } from '@/app/utils/helpers/generate.random-string';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa'; // Import bookmark icons
 import { CampaignResponseDataType } from '../types/campaigns.types';
 import EmptyPage from '../components/emptypage/EmptyPage';
 
 const Favorites = () => {
-  const { campaigns, loading, error, fetchFavoritedCampaigns } =
-    useCampaignContext();
+  const {
+    campaigns,
+    loading,
+    error,
+    fetchFavoritedCampaigns,
+    favoriteCampaign,
+    unfavoriteCampaign,
+  } = useCampaignContext();
 
   // Trigger fetch for favorited campaigns when the component mounts
   useEffect(() => {
@@ -31,6 +36,15 @@ const Favorites = () => {
   if (campaigns.length === 0) {
     return <EmptyPage />;
   }
+
+  // Handle favorite/unfavorite action
+  const handleFavorite = async (campaignId: string) => {
+    await favoriteCampaign(campaignId);
+  };
+
+  const handleUnfavorite = async (campaignId: string) => {
+    await unfavoriteCampaign(campaignId);
+  };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
@@ -60,6 +74,22 @@ const Favorites = () => {
                 </div>
               </div>
             </Link>
+            {/* Favorite/Unfavorite Icon */}
+            <div
+              className="absolute top-2 right-2 p-2 bg-transparent rounded-full shadow-md cursor-pointer hover:bg-gray-100"
+              onClick={(e) => {
+                e.preventDefault(); // Prevent link navigation
+                campaign.favorited
+                  ? handleUnfavorite(campaign.id.toString())
+                  : handleFavorite(campaign.id.toString());
+              }}
+            >
+              {campaign.favorited ? (
+                <FaBookmark className="text-orange-500" />
+              ) : (
+                <FaRegBookmark className="text-gray-50" />
+              )}
+            </div>
           </div>
         ))}
     </div>
