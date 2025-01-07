d6a1f75efec3e9b062a096178d44b28354b815a9
josephadeabah <abeansah.lx@gmail.com>
2025-01-07 17:35:03 +0000

    fix: fetch transfers for user

--- frontend/app/account/Transfers.tsx
+++ frontend/app/account/Transfers.tsx
@@ -177,7 +177,7 @@ export default function Transfers() {
                     className="px-4 py-2 bg-green-400 text-white rounded-full hover:bg-green-600 dark:hover:bg-green-700"
                     disabled={
                       loadingCampaigns[campaign.id] ||
-                      parseFloat(campaign.current_amount) < 60.0
+                      parseFloat(campaign.current_amount) < 50.0
                     }
                   >
                     {loadingCampaigns[campaign.id]
