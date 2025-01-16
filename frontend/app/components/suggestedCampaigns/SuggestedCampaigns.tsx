9c9bf02cb82a0ededc1384817c9f9b7e73695450
josephadeabah <abeansah.lx@gmail.com>
2025-01-16 21:14:27 +0000

    feat: add suggested fundraisers

--- frontend/app/components/suggestedCampaigns/SuggestedCampaigns.tsx
+++ frontend/app/components/suggestedCampaigns/SuggestedCampaigns.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 import { useCampaignContext } from '@/app/context/account/campaign/CampaignsContext';
-import CampaignCardLoader from '@/app/loaders/CampaignCardLoader';
 import { useUserContext } from '@/app/context/users/UserContext';
 import { motion } from 'framer-motion';
 import Progress from '@/app/components/progressbar/ProgressBar';
@@ -15,13 +14,8 @@ import { useAuth } from '@/app/context/auth/AuthContext';
 import ToastComponent from '@/app/components/toast/Toast';
 
 const SuggestedCampaignsComponent = () => {
-  const {
-    fetchAllCampaigns,
-    campaigns,
-    loading,
-    favoriteCampaign,
-    unfavoriteCampaign,
-  } = useCampaignContext();
+  const { fetchAllCampaigns, campaigns, favoriteCampaign, unfavoriteCampaign } =
+    useCampaignContext();
   const { user } = useAuth();
   const [sortBy, setSortBy] = useState<string>('created_at');
   const [sortOrder, setSortOrder] = useState<string>('desc');
@@ -31,7 +25,6 @@ const SuggestedCampaignsComponent = () => {
   const [dateRange, setDateRange] = useState<string>('all_time');
   const [goalRange, setGoalRange] = useState<string>('all');
   const [location, setLocation] = useState<string>('all');
-  const { pagination } = useCampaignContext();
 
   const [toast, setToast] = useState({
     isOpen: false,
@@ -102,103 +95,94 @@ const SuggestedCampaignsComponent = () => {
 
   return (
     <div className="w-full bg-white md:p-4">
-      {loading ? (
-        <CampaignCardLoader />
-      ) : (
-        <>
-          {campaigns && campaigns.length > 0 ? (
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 px-4">
-              {campaigns
-                .filter((campaign) => campaign.permissions.is_public)
-                .map((campaign, index) => {
-                  const fundraiserCurrency =
-                    campaign?.currency_symbol ||
-                    campaign?.currency?.toUpperCase();
+      {campaigns && campaigns.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 px-4">
+          {campaigns
+            .filter((campaign) => campaign.permissions.is_public)
+            .map((campaign, index) => {
+              const fundraiserCurrency =
+                campaign?.currency_symbol || campaign?.currency?.toUpperCase();
 
-                  return (
-                    <motion.div
-                      key={campaign.id}
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.5, delay: index * 0.1 }}
-                      className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300"
-                    >
-                      <Link
-                        href={`/campaign/${campaign.id}?${generateRandomString()}`}
-                      >
-                        <div>
-                          <div
-                            className="relative w-full"
-                            style={{ paddingTop: '56.25%' }}
-                          >
-                            <Image
-                              src={campaign?.media || '/bantuhive.svg'}
-                              alt="media thumbnail"
-                              fill
-                              className="absolute top-0 left-0 object-cover"
-                            />
-                          </div>
-                          <div className="p-4">
-                            <h3 className="text-lg font-semibold truncate">
-                              {campaign?.title}
-                            </h3>
-                            <div className="text-sm text-gray-500">
-                              {deslugify(campaign?.category)}
-                            </div>
-                            <div className="flex justify-between text-sm text-gray-400 mt-2">
-                              <span>{campaign.total_donors || 0} Backers</span>
-                              <span>{campaign.remaining_days} days left</span>
-                            </div>
-                            <Progress
-                              firstProgress={
-                                (Number(campaign?.transferred_amount) /
-                                  Number(campaign?.goal_amount)) *
-                                100
-                              }
-                              firstTooltipContent={`Progress: ${
-                                (Number(campaign?.transferred_amount) /
-                                  Number(campaign?.goal_amount)) *
-                                100
-                              }%`}
-                            />
-                            <div className="text-sm text-gray-600 mt-2">
-                              {fundraiserCurrency}
-                              {parseFloat(
-                                campaign.transferred_amount,
-                              ).toLocaleString()}{' '}
-                              of {fundraiserCurrency}
-                              {parseFloat(
-                                campaign.goal_amount,
-                              ).toLocaleString()}
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
+              return (
+                <motion.div
+                  key={campaign.id}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                  className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300"
+                >
+                  <Link
+                    href={`/campaign/${campaign.id}?${generateRandomString()}`}
+                  >
+                    <div>
                       <div
-                        className="absolute top-3 right-3 p-2 bg-white rounded-full shadow-md cursor-pointer hover:bg-gray-100"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          campaign.favorited
-                            ? handleUnfavorite(campaign.id.toString())
-                            : handleFavorite(campaign.id.toString());
-                        }}
+                        className="relative w-full"
+                        style={{ paddingTop: '56.25%' }}
                       >
-                        {campaign.favorited ? (
-                          <FaBookmark className="text-orange-500" />
-                        ) : (
-                          <FaRegBookmark className="text-gray-500" />
-                        )}
+                        <Image
+                          src={campaign?.media || '/bantuhive.svg'}
+                          alt="media thumbnail"
+                          fill
+                          className="absolute top-0 left-0 object-cover"
+                        />
                       </div>
-                    </motion.div>
-                  );
-                })}
-            </div>
-          ) : (
-            <p className="text-center text-gray-500">
-              No campaigns found. Try adjusting your filters or search terms.
-            </p>
-          )}
-        </>
+                      <div className="p-4">
+                        <h3 className="text-lg font-semibold truncate">
+                          {campaign?.title}
+                        </h3>
+                        <div className="text-sm text-gray-500">
+                          {deslugify(campaign?.category)}
+                        </div>
+                        <div className="flex justify-between text-sm text-gray-400 mt-2">
+                          <span>{campaign.total_donors || 0} Backers</span>
+                          <span>{campaign.remaining_days} days left</span>
+                        </div>
+                        <Progress
+                          firstProgress={
+                            (Number(campaign?.transferred_amount) /
+                              Number(campaign?.goal_amount)) *
+                            100
+                          }
+                          firstTooltipContent={`Progress: ${
+                            (Number(campaign?.transferred_amount) /
+                              Number(campaign?.goal_amount)) *
+                            100
+                          }%`}
+                        />
+                        <div className="text-sm text-gray-600 mt-2">
+                          {fundraiserCurrency}
+                          {parseFloat(
+                            campaign.transferred_amount,
+                          ).toLocaleString()}{' '}
+                          of {fundraiserCurrency}
+                          {parseFloat(campaign.goal_amount).toLocaleString()}
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                  <div
+                    className="absolute top-3 right-3 p-2 bg-white rounded-full shadow-md cursor-pointer hover:bg-gray-100"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      campaign.favorited
+                        ? handleUnfavorite(campaign.id.toString())
+                        : handleFavorite(campaign.id.toString());
+                    }}
+                  >
+                    {campaign.favorited ? (
+                      <FaBookmark className="text-orange-500" />
+                    ) : (
+                      <FaRegBookmark className="text-gray-500" />
+                    )}
+                  </div>
+                </motion.div>
+              );
+            })}
+        </div>
+      ) : (
+        <p className="text-center text-gray-500">
+          No campaigns found. Try adjusting your filters or search terms.
+        </p>
       )}
       {toast.isOpen && (
         <ToastComponent
