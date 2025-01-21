0a459f3de36cc215a9e00bf043e208f821ddd0d7
josephadeabah <abeansah.lx@gmail.com>
2025-01-21 20:02:31 +0000

    fix: redesign campaign cards

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -119,112 +119,112 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
   if (error) return <ErrorPage />;
 
   return (
-    <div className="w-full max-w-7xl mx-auto bg-white md:p-4">
-  {/* Toast component for notifications */}
-  <ToastComponent
-    isOpen={toast.isOpen}
-    onClose={() => setToast((prev) => ({ ...prev, isOpen: false }))}
-    title={toast.title}
-    description={toast.description}
-    type={toast.type}
-  />
-  {filteredCampaigns.length === 0 ? (
-    <EmptyPage />
-  ) : (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-4 relative">
-      {filteredCampaigns?.map((campaign, index) => {
-        const fundraiserCurrency =
-          campaign?.currency_symbol || campaign?.currency?.toUpperCase();
+    <div className="w-full max-w-7xl mx-auto bg-gray-50 md:p-4">
+      {/* Toast component for notifications */}
+      <ToastComponent
+        isOpen={toast.isOpen}
+        onClose={() => setToast((prev) => ({ ...prev, isOpen: false }))}
+        title={toast.title}
+        description={toast.description}
+        type={toast.type}
+      />
+      {filteredCampaigns.length === 0 ? (
+        <EmptyPage />
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-4 relative">
+          {filteredCampaigns?.map((campaign, index) => {
+            const fundraiserCurrency =
+              campaign?.currency_symbol || campaign?.currency?.toUpperCase();
 
-        return (
-          <motion.div
-            key={campaign.id}
-            initial="hidden"
-            animate="visible"
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 rounded-lg shadow-md transform hover:scale-105 hover:shadow-xl transition-transform duration-300 cursor-pointer overflow-hidden"
-          >
-            <Link
-              href={`/campaign/${campaign.id}?${generateRandomString()}`}
-            >
-              <div className="relative w-full h-0 pb-[100%]">
-                <Image
-                  src={campaign?.media || '/bantuhive.svg'}
-                  alt="media thumbnail"
-                  layout="fill"
-                  objectFit="cover"
-                  className="absolute top-0 left-0 w-full h-full rounded-t"
-                />
-                {/* Hover Overlay */}
-                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
-                  <h3 className="text-lg font-bold text-white truncate mb-1">
-                    {campaign?.title}
-                  </h3>
-                  <div className="text-sm text-orange-400 truncate mb-1">
-                    {deslugify(campaign?.category)}
+            return (
+              <motion.div
+                key={campaign.id}
+                initial="hidden"
+                animate="visible"
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+                className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 rounded-lg shadow-md transform hover:scale-105 hover:shadow-xl transition-transform duration-300 cursor-pointer overflow-hidden"
+              >
+                <Link
+                  href={`/campaign/${campaign.id}?${generateRandomString()}`}
+                >
+                  <div className="relative w-full h-0 pb-[100%]">
+                    <Image
+                      src={campaign?.media || '/bantuhive.svg'}
+                      alt="media thumbnail"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute top-0 left-0 w-full h-full rounded-t"
+                    />
+                    {/* Hover Overlay */}
+                    <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+                      <h3 className="text-lg font-bold text-white truncate mb-1">
+                        {campaign?.title}
+                      </h3>
+                      <div className="text-sm text-orange-400 truncate mb-1">
+                        {deslugify(campaign?.category)}
+                      </div>
+                      <div className="flex justify-between items-center text-xs font-semibold text-gray-300 mb-2">
+                        <span>{campaign.total_donors || 0} Backers</span>
+                        <span>{campaign.remaining_days} days left</span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex justify-between items-center text-xs font-semibold text-gray-300 mb-2">
-                    <span>{campaign.total_donors || 0} Backers</span>
-                    <span>{campaign.remaining_days} days left</span>
+                  <div className="px-4 py-2 dark:text-gray-50">
+                    <div className="w-full text-xs">
+                      <Progress
+                        firstProgress={
+                          (Number(campaign?.transferred_amount) /
+                            Number(campaign?.goal_amount)) *
+                          100
+                        }
+                        firstTooltipContent={`Progress: ${
+                          (Number(campaign?.transferred_amount) /
+                            Number(campaign?.goal_amount)) *
+                          100
+                        }%`}
+                      />
+                    </div>
+                    <div className="w-full text-xs text-gray-600 flex flex-col">
+                      <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
+                        {fundraiserCurrency}
+                        {!isNaN(parseFloat(campaign.transferred_amount))
+                          ? parseFloat(
+                              campaign.transferred_amount,
+                            ).toLocaleString()
+                          : 0}
+                        <span className="text-gray-600 dark:text-gray-100 truncate">
+                          <span className="text-xs p-1">of</span>
+                          {fundraiserCurrency}
+                          {parseFloat(campaign.goal_amount).toLocaleString()}
+                        </span>
+                      </p>
+                    </div>
                   </div>
+                </Link>
+                {/* Bookmark Icon */}
+                <div
+                  className="absolute top-2 right-2 p-2 bg-transparent rounded-full shadow-md cursor-pointer hover:bg-gray-100"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    campaign.favorited
+                      ? handleUnfavorite(campaign.id.toString())
+                      : handleFavorite(campaign.id.toString());
+                  }}
+                >
+                  {campaign.favorited ? (
+                    <FaBookmark className="text-orange-500" />
+                  ) : (
+                    <FaRegBookmark className="text-gray-50" />
+                  )}
                 </div>
-              </div>
-              <div className="px-4 py-2 dark:text-gray-50">
-                <div className="w-full text-xs">
-                  <Progress
-                    firstProgress={
-                      (Number(campaign?.transferred_amount) /
-                        Number(campaign?.goal_amount)) *
-                      100
-                    }
-                    firstTooltipContent={`Progress: ${
-                      (Number(campaign?.transferred_amount) /
-                        Number(campaign?.goal_amount)) *
-                      100
-                    }%`}
-                  />
-                </div>
-                <div className="w-full text-xs text-gray-600 flex flex-col">
-                  <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
-                    {fundraiserCurrency}
-                    {!isNaN(parseFloat(campaign.transferred_amount))
-                      ? parseFloat(
-                          campaign.transferred_amount,
-                        ).toLocaleString()
-                      : 0}
-                    <span className="text-gray-600 dark:text-gray-100 truncate">
-                      <span className="text-xs p-1">of</span>
-                      {fundraiserCurrency}
-                      {parseFloat(campaign.goal_amount).toLocaleString()}
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </Link>
-            {/* Bookmark Icon */}
-            <div
-              className="absolute top-2 right-2 p-2 bg-transparent rounded-full shadow-md cursor-pointer hover:bg-gray-100"
-              onClick={(e) => {
-                e.preventDefault();
-                campaign.favorited
-                  ? handleUnfavorite(campaign.id.toString())
-                  : handleFavorite(campaign.id.toString());
-              }}
-            >
-              {campaign.favorited ? (
-                <FaBookmark className="text-orange-500" />
-              ) : (
-                <FaRegBookmark className="text-gray-50" />
-              )}
-            </div>
-          </motion.div>
-        );
-      })}
-    </div>
-  )}
+              </motion.div>
+            );
+          })}
+        </div>
+      )}
 
-  {/* Commenting the Pagination here for now */}
-  {/* <div className="p-3">
+      {/* Commenting the Pagination here for now */}
+      {/* <div className="p-3">
   {pagination.totalPages > 1 && (
     <Pagination
       currentPage={pagination.currentPage || page}
@@ -233,8 +233,7 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
     />
   )}
   </div> */}
-</div>
-
+    </div>
   );
 };
 
