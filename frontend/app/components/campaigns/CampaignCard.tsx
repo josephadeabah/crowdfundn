e34a3d09f1a06ee1871744035d395aa8489c6a7b
josephadeabah <abeansah.lx@gmail.com>
2025-01-21 19:55:10 +0000

    fix: redesign campaign cards

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -142,7 +142,7 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
             initial="hidden"
             animate="visible"
             transition={{ duration: 0.5, delay: index * 0.1 }}
-            className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 rounded-lg shadow-lg transform hover:scale-105 hover:shadow-xl transition-transform duration-300 cursor-pointer overflow-hidden"
+            className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 rounded-lg shadow-md transform hover:scale-105 hover:shadow-xl transition-transform duration-300 cursor-pointer overflow-hidden"
           >
             <Link
               href={`/campaign/${campaign.id}?${generateRandomString()}`}
