80ded81bbc729ae9ffa04fcf7ab85f3190a5180d
josephadeabah <abeansah.lx@gmail.com>
2025-01-23 15:36:14 +0000

    fix: max campaign

--- frontend/app/explore/advance/page.tsx
+++ frontend/app/explore/advance/page.tsx
@@ -206,8 +206,8 @@ const CampaignsPage = () => {
         </motion.h4>
         {/* Search Section */}
         <div className="flex justify-center mb-1">
-          <div className="w-full max-w-2xl mx-auto flex p-6">
-            <div className="relative flex items-end w-full">
+          <div className="w-full max-w-2xl sm:mx-auto flex justify-end p-6">
+            <div className="relative flex items-center w-full">
               <input
                 id="search"
                 type="text"
