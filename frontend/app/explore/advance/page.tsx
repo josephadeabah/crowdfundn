95f54fe1897caaee28f8a7ca8747e1415cd5161e
josephadeabah <abeansah.lx@gmail.com>
2025-01-24 21:47:46 +0000

    fix: add campaign title to header

--- frontend/app/explore/advance/page.tsx
+++ frontend/app/explore/advance/page.tsx
@@ -424,9 +424,11 @@ const CampaignsPage = () => {
                                 />
                               </div>
                               <div className="w-full text-xs text-gray-600 flex flex-col">
+                                <head>
                                 <h3 className="text-lg font-bold text-gray-700 truncate mb-1">
                                   {campaign?.title}
                                 </h3>
+                                </head>
                                 <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
                                   {fundraiserCurrency}
                                   {!isNaN(
