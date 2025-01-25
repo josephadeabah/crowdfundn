9444c0c872134a43e3b9b6136f84a8c5d58161e4
josephadeabah <abeansah.lx@gmail.com>
2025-01-25 14:35:30 +0000

    chore: only five loading cards

--- frontend/app/loaders/CampaignCardLoader.tsx
+++ frontend/app/loaders/CampaignCardLoader.tsx
@@ -4,7 +4,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const CampaignCardLoader = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2 px-2 md:p-0">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 px-2 md:p-0">
       {Array(12)
         .fill(null)
         .map((_, index) => (
