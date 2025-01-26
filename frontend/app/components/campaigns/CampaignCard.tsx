6a2df66401137de2d6c20d309f87feed6d3fcb7f
josephadeabah <abeansah.lx@gmail.com>
2025-01-26 10:25:00 +0000

    fix: add more mock data to leaderboard

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -119,7 +119,7 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
   if (error) return <ErrorPage />;
 
   return (
-    <div className="w-full max-w-7xl mx-auto bg-white md:p-4">
+    <div className="w-full max-w-7xl mx-auto bg-white md:p-0">
       {/* Toast component for notifications */}
       <ToastComponent
         isOpen={toast.isOpen}
