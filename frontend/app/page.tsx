d453aaec10bb50ea646f4b0dbbb67c641b2d4054
josephadeabah <abeansah.lx@gmail.com>
2025-01-26 12:35:15 +0000

    fix: card

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -266,7 +266,7 @@ const HomePage = () => {
           <h2 className="text-4xl font-bold mb-8 mt-4 text-center">
             Fundraising Now
           </h2>
-          <div className="w-full bg-white">
+          <div className="w-full bg-white px-4">
             <CampaignCard
               campaigns={campaigns}
               loading={loading}
