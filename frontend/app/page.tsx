fe6caaad5648970929c8a0667496842c3ee2d148
josephadeabah <abeansah.lx@gmail.com>
2025-01-26 10:44:21 +0000

    fix: add more mock data to leaderboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -266,7 +266,7 @@ const HomePage = () => {
           <h2 className="text-4xl font-bold mb-8 text-center">
             Fundraising Now
           </h2>
-          <div className="w-full max-w-7xl mx-auto">
+          <div className="w-full bg-white">
             <CampaignCard
               campaigns={campaigns}
               loading={loading}
