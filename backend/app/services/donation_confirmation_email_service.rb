b09aad4f0403ccfda19b1d5d25490ad631ff1d29
josephadeabah <abeansah.lx@gmail.com>
2025-01-28 14:16:51 +0000

    feat: add donation notification for fundraiser

--- backend/app/services/donation_confirmation_email_service.rb
+++ backend/app/services/donation_confirmation_email_service.rb
@@ -9,7 +9,7 @@ class DonationConfirmationEmailService
       transaction_reference = donation.transaction_reference
       transaction_amount = donation.gross_amount.to_f
       transaction_date = donation.created_at.strftime('%B %d, %Y')
-      currency_symbol = donation.campaign.currency_symbol
+      currency_symbol = donation.metadata.campaign_metadata.currency
   
       send_smtp_email = SibApiV3Sdk::SendSmtpEmail.new(
         to: [
