31e1bdb346c0e28a139dfc9b5d53231de4875d35
josephadeabah <abeansah.lx@gmail.com>
2025-01-30 09:56:26 +0000

    chore: clean code

--- frontend/app/leaderboard/backers/page.tsx
+++ frontend/app/leaderboard/backers/page.tsx
@@ -147,7 +147,7 @@ const LeaderboardBackersPage = () => {
                       </td>
                       <td className="px-4 py-2 text-gray-700">
                         {backer?.currency?.toUpperCase()}{' '}
-                        {backer?.total_donations}
+                        {Math.round(backer?.total_donations) || 'N/A'}
                       </td>
                       <td className="px-4 py-2 text-gray-700">
                         {backer.score || 'N/A'}
