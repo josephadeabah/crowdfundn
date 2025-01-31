498dbc123ef42d115aa191c76017b802ec39c000
josephadeabah <abeansah.lx@gmail.com>
2025-01-31 23:37:03 +0000

    fix: why choose us desc

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -207,11 +207,11 @@ const HomePage = () => {
               initial="hidden"
               animate={controls}
               transition={{ duration: 0.5 }}
-              className="text-4xl font-bold text-center mb-12"
+              className="text-4xl font-bold text-center mb-6"
             >
               Why Choose Us?
             </motion.h2>
-            <div className="text-2xl text-cyan-500 font-bold text-center py-3">
+            <div className="text-2xl text-green-600 font-bold text-center mb-4">
               We're Africa's Premier And Most Trusted Reward-based And Gamified
               Crowdfunding Platform
             </div>
