dd5b1c141f3826c6fc6004f02beb20fa09f3fe59
josephadeabah <abeansah.lx@gmail.com>
2025-02-01 13:01:26 +0000

    fix: add image validator gem

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -14,12 +14,8 @@ class Profile < ApplicationRecord
   validates :category, presence: true, allow_blank: true
   validates :location, presence: true, allow_blank: true
 
-  # Avatar validations (Optional or Required)
+  # Avatar validations using active_storage_validations gem
   validates :avatar, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 5.megabytes }
-
-  # Custom validation for avatar file type and size
-  validate :avatar_content_type
-  validate :avatar_file_size
 
   # Method to return avatar URL
   def avatar_url
@@ -33,25 +29,5 @@ class Profile < ApplicationRecord
   # Ensure avatar URL is included in JSON response
   def as_json(options = {})
     super(options.merge(methods: [:avatar_url]))
-  end
-
-  private
-
-  # Validate the content type of the avatar
-  def avatar_content_type
-    return unless avatar.attached?
-
-    unless avatar.content_type.in?(%w[image/jpeg image/png image/jpg])
-      errors.add(:avatar, 'must be a JPEG, PNG, or JPG')
-    end
-  end
-
-  # Validate the file size of the avatar
-  def avatar_file_size
-    return unless avatar.attached?
-
-    if avatar.byte_size > 5.megabytes
-      errors.add(:avatar, 'should be less than 5MB')
-    end
   end
 end
