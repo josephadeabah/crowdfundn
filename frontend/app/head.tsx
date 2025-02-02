635bbf17ff0646574ba627b42361e94ab4660c76
josephadeabah <abeansah.lx@gmail.com>
2025-02-02 21:55:38 +0000

    fix: seo title tag

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -1,94 +1,92 @@
 export default function Head() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Fundraising Made Easy for Africa & Diaspora</title>
-        <meta
-          name="description"
-          content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
-        />
-        <meta
-          name="keywords"
-          content="fundraising, Africa, diaspora, charity, donations, nonprofit, crowdfunding africawerise africarising startup crowdfundingstartup fintech"
-        />
-        <meta
-          property="og:title"
-          content="Fundraising Made Easy for Africa & Diaspora"
-        />
-        <meta
-          property="og:description"
-          content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
-        />
-        <meta
-          property="og:image"
-          content="https://bantuhive.com/marketing7.png"
-        />
-        <meta property="og:url" content="https://bantuhive.com" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:site_name"
-          content="Bantu Hive - Fundraising Made Easy"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@bantuhive" />
-        <meta
-          name="twitter:description"
-          content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
-        />
-        <meta
-          name="twitter:image"
-          content="https://bantuhive.com/marketing6.png"
-        />
-        <meta
-          name="twitter:title"
-          content="Fundraising Made Easy for Africa & Diaspora"
-        />
-        <link rel="canonical" href="https://bantuhive.com" />
-        <link rel="icon" href="/bantuhive.ico" type="image/x-icon" />
-        <link rel="icon" href="/bantuhive.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/bantuhive.png" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org',
-              '@type': 'Organization',
-              name: 'Bantu Hive',
-              url: 'https://bantuhive.com',
-              logo: 'https://bantuhive.com/bantuhive.ico',
-              description:
-                'BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora.',
-              foundingDate: '2024',
-              foundingLocation: {
-                '@type': 'Place',
-                name: 'Africa',
-              },
-              contactPoint: {
-                '@type': 'ContactPoint',
-                telephone: '+233200415683',
-                contactType: 'customer service',
-                email: 'help@bantuhive.com',
-                areaServed: 'Africa, Diaspora',
-              },
-              address: {
-                '@type': 'PostalAddress',
-                streetAddress: 'IVY Street, Kingstel Hotel Avenue',
-                addressLocality: 'Takoradi',
-                addressRegion: 'Western Region',
-                postalCode: '982',
-                addressCountry: 'Ghana',
-              },
-              sameAs: [
-                'https://web.facebook.com/profile.php?id=61568192851056',
-                'https://www.instagram.com/bantuhive_fund/',
-                'https://www.linkedin.com/company/bantu-hive/about/',
-              ],
-            }),
-          }}
-        />
-      </head>
-    </html>
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Fundraising Made Easy for Africa & Diaspora</title>
+      <meta
+        name="description"
+        content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
+      />
+      <meta
+        name="keywords"
+        content="fundraising, Africa, diaspora, charity, donations, nonprofit, crowdfunding africawerise africarising startup crowdfundingstartup fintech"
+      />
+      <meta
+        property="og:title"
+        content="Fundraising Made Easy for Africa & Diaspora"
+      />
+      <meta
+        property="og:description"
+        content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
+      />
+      <meta
+        property="og:image"
+        content="https://bantuhive.com/marketing7.png"
+      />
+      <meta property="og:url" content="https://bantuhive.com" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:site_name"
+        content="Bantu Hive - Fundraising Made Easy"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@bantuhive" />
+      <meta
+        name="twitter:description"
+        content="BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora."
+      />
+      <meta
+        name="twitter:image"
+        content="https://bantuhive.com/marketing6.png"
+      />
+      <meta
+        name="twitter:title"
+        content="Fundraising Made Easy for Africa & Diaspora"
+      />
+      <link rel="canonical" href="https://bantuhive.com" />
+      <link rel="icon" href="/bantuhive.ico" type="image/x-icon" />
+      <link rel="icon" href="/bantuhive.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/bantuhive.png" />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'Organization',
+            name: 'Bantu Hive',
+            url: 'https://bantuhive.com',
+            logo: 'https://bantuhive.com/bantuhive.ico',
+            description:
+              'BantuHive operates as a premier reward-based and gamified crowdfunding platform focused on facilitating fundraising efforts across Africa and the Diaspora.',
+            foundingDate: '2024',
+            foundingLocation: {
+              '@type': 'Place',
+              name: 'Africa',
+            },
+            contactPoint: {
+              '@type': 'ContactPoint',
+              telephone: '+233200415683',
+              contactType: 'customer service',
+              email: 'help@bantuhive.com',
+              areaServed: 'Africa, Diaspora',
+            },
+            address: {
+              '@type': 'PostalAddress',
+              streetAddress: 'IVY Street, Kingstel Hotel Avenue',
+              addressLocality: 'Takoradi',
+              addressRegion: 'Western Region',
+              postalCode: '982',
+              addressCountry: 'Ghana',
+            },
+            sameAs: [
+              'https://web.facebook.com/profile.php?id=61568192851056',
+              'https://www.instagram.com/bantuhive_fund/',
+              'https://www.linkedin.com/company/bantu-hive/about/',
+            ],
+          }),
+        }}
+      />
+    </head>
   );
 }
