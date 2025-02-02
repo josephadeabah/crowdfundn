60fc0b3b1f432f6ca372cdf907ead76152333f5d
josephadeabah <abeansah.lx@gmail.com>
2025-02-02 09:31:31 +0000

    fix: add default img to avatar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -243,7 +243,7 @@ const Navbar = () => {
                         imageUrl={
                           String(
                             userAccountData?.profile?.avatar?.record?.avatar,
-                          ) || '/bantuhive.svg'
+                          ) || '/avatar-default1.png'
                         }
                       />
                     </Link>
@@ -306,7 +306,7 @@ const Navbar = () => {
                       imageUrl={
                         String(
                           userAccountData?.profile?.avatar?.record?.avatar,
-                        ) || '/bantuhive.svg'
+                        ) || '/avatar-default1.png'
                       }
                     />
                   </div>
@@ -320,7 +320,7 @@ const Navbar = () => {
                           size="sm"
                           imageUrl={String(
                             userAccountData?.profile?.avatar?.record?.avatar,
-                          ) || '/bantuhive.svg'}
+                          ) || '/avatar-default1.png'}
                         />
                       </Link>
                       <div
