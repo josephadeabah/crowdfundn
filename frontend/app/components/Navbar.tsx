6fc774a620584736f9c4e8e8fdf1fb316c2ab3bc
josephadeabah <abeansah.lx@gmail.com>
2025-02-03 12:49:30 +0000

    fix: row rendering for campaign cards

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -243,7 +243,7 @@ const Navbar = () => {
                         imageUrl={
                           String(
                             userAccountData?.profile?.avatar?.record?.avatar,
-                          ) || '/avatar-default1.png'
+                          )
                         }
                       />
                     </Link>
@@ -306,7 +306,7 @@ const Navbar = () => {
                       imageUrl={
                         String(
                           userAccountData?.profile?.avatar?.record?.avatar,
-                        ) || '/avatar-default1.png'
+                        )
                       }
                     />
                   </div>
@@ -321,7 +321,7 @@ const Navbar = () => {
                           imageUrl={
                             String(
                               userAccountData?.profile?.avatar?.record?.avatar,
-                            ) || '/avatar-default1.png'
+                            )
                           }
                         />
                       </Link>
