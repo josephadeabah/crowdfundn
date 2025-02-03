677f8505aa6979461491a31454541133617a7296
josephadeabah <abeansah.lx@gmail.com>
2025-02-03 12:47:11 +0000

    fix: row rendering for campaign cards

--- frontend/app/components/avatar/Avatar.tsx
+++ frontend/app/components/avatar/Avatar.tsx
@@ -99,7 +99,7 @@ const Avatar: React.FC<AvatarProps> = ({ name='User', imageUrl, size = 'md' }) =
     >
       {imageUrl ? (
         <img
-          src={imageUrl === "" ? '/avatar-default1.png' : imageUrl}
+          src={imageUrl === null ? '/avatar-default1.png' : imageUrl}
           alt={`${name}'s avatar`}
           className="w-full h-full object-cover"
           loading="lazy"
