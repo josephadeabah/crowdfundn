399aefb4ee2a940461e51c388c515d55630dc731
josephadeabah <abeansah.lx@gmail.com>
2025-02-04 04:54:40 +0000

    fix: make donation id null in points

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -284,7 +284,7 @@ const SingleCampaignPage: React.FC = () => {
                     onClick={handleShare}
                     className="flex items-center justify-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
                   >
-                    <FaShare className="mr-2" /> {currentCampaign?.total_shares}{' '}
+                    <FaShare className="mr-2" /> {currentCampaign?.total_shares || 0}{' '}
                     Shares
                   </Button>
                   <button
