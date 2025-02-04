9125d22ea4355cdfc82bb8711c93d2f985465068
josephadeabah <abeansah.lx@gmail.com>
2025-02-04 00:08:11 +0000

    feat: add level to backer leaderboard

--- frontend/app/account/Rewards.tsx
+++ frontend/app/account/Rewards.tsx
@@ -198,7 +198,7 @@ const RewardsPage: React.FC = () => {
     }
 
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
+      <div className="grid grid-cols-1 justify-items-center">
         {levels.map((rewardLevel) => (
           <div
             key={rewardLevel}
