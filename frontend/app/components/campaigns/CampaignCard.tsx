e2b6097785dad64c1ee3fe059d31e957cbd8cb3c
josephadeabah <abeansah.lx@gmail.com>
2025-02-05 11:31:49 +0000

    chore: run format

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -119,120 +119,122 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
   if (error) return <ErrorPage />;
 
   return (
-<div className="w-full max-w-7xl mx-auto p-6 bg-white rounded-lg shadow-sm">
-  <ToastComponent
-    isOpen={toast.isOpen}
-    onClose={() => setToast((prev) => ({ ...prev, isOpen: false }))}
-    title={toast.title}
-    description={toast.description}
-    type={toast.type}
-  />
-  {filteredCampaigns.length === 0 ? (
-    <EmptyPage />
-  ) : (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6">
-      {filteredCampaigns.slice(0, 9).map((campaign, index) => {
-        const fundraiserCurrency =
-          campaign?.currency_symbol || campaign?.currency?.toUpperCase();
+    <div className="w-full max-w-7xl mx-auto p-6 bg-white rounded-lg shadow-sm">
+      <ToastComponent
+        isOpen={toast.isOpen}
+        onClose={() => setToast((prev) => ({ ...prev, isOpen: false }))}
+        title={toast.title}
+        description={toast.description}
+        type={toast.type}
+      />
+      {filteredCampaigns.length === 0 ? (
+        <EmptyPage />
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {filteredCampaigns.slice(0, 9).map((campaign, index) => {
+            const fundraiserCurrency =
+              campaign?.currency_symbol || campaign?.currency?.toUpperCase();
 
-        let colSpan = 1;
-        let rowSpan = 1;
-        if (index === 0) {
-          colSpan = 2;
-          rowSpan = 2;
-        } else if (index === 4 || index === 5 || index === 6) {
-          colSpan = 1;
-          rowSpan = 1;
-        }
+            let colSpan = 1;
+            let rowSpan = 1;
+            if (index === 0) {
+              colSpan = 2;
+              rowSpan = 2;
+            } else if (index === 4 || index === 5 || index === 6) {
+              colSpan = 1;
+              rowSpan = 1;
+            }
 
-        return (
-          <motion.div
-            key={campaign.id}
-            initial="hidden"
-            animate="visible"
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            className={`group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 hover:shadow-2xl transition-transform duration-300 cursor-pointer overflow-hidden rounded-lg ${
-              index === 0 ? 'col-span-2 row-span-2' : ''
-            } ${index === 4 || index === 5 || index === 6 ? 'col-span-1' : ''}`}
-          >
-            <Link href={`/campaign/${campaign.id}?${generateRandomString()}`}>
-              <div className="relative w-full h-0 pb-[100%]">
-                <Image
-                  src={campaign?.media || '/bantuhive.svg'}
-                  alt="media thumbnail"
-                  layout="fill"
-                  objectFit="cover"
-                  className="absolute top-0 left-0 w-full h-full"
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
-                  <h3 className="text-lg font-bold text-white truncate mb-1">
-                    {campaign?.title}
-                  </h3>
-                  <div className="text-sm text-orange-400 truncate mb-1">
-                    {deslugify(campaign?.category)}
+            return (
+              <motion.div
+                key={campaign.id}
+                initial="hidden"
+                animate="visible"
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+                className={`group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 hover:shadow-2xl transition-transform duration-300 cursor-pointer overflow-hidden rounded-lg ${
+                  index === 0 ? 'col-span-2 row-span-2' : ''
+                } ${index === 4 || index === 5 || index === 6 ? 'col-span-1' : ''}`}
+              >
+                <Link
+                  href={`/campaign/${campaign.id}?${generateRandomString()}`}
+                >
+                  <div className="relative w-full h-0 pb-[100%]">
+                    <Image
+                      src={campaign?.media || '/bantuhive.svg'}
+                      alt="media thumbnail"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute top-0 left-0 w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+                      <h3 className="text-lg font-bold text-white truncate mb-1">
+                        {campaign?.title}
+                      </h3>
+                      <div className="text-sm text-orange-400 truncate mb-1">
+                        {deslugify(campaign?.category)}
+                      </div>
+                      <div className="flex justify-between items-center text-xs font-semibold text-gray-300 mb-2">
+                        <span>{campaign.total_donors || 0} Backers</span>
+                        <span>{campaign.remaining_days} days left</span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex justify-between items-center text-xs font-semibold text-gray-300 mb-2">
-                    <span>{campaign.total_donors || 0} Backers</span>
-                    <span>{campaign.remaining_days} days left</span>
+                  <div className="px-4 py-3 h-full bg-gray-50 dark:bg-gray-800">
+                    <div className="w-full text-xs">
+                      <Progress
+                        firstProgress={
+                          (Number(campaign?.transferred_amount) /
+                            Number(campaign?.goal_amount)) *
+                          100
+                        }
+                        firstTooltipContent={`Progress: ${
+                          (Number(campaign?.transferred_amount) /
+                            Number(campaign?.goal_amount)) *
+                          100
+                        }%`}
+                      />
+                    </div>
+                    <div className="w-full text-xs text-gray-600 dark:text-gray-300 flex flex-col">
+                      <h3 className="text-lg font-bold text-gray-700 dark:text-gray-100 truncate mb-1">
+                        {campaign?.title}
+                      </h3>
+                      <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
+                        {fundraiserCurrency}
+                        {!isNaN(parseFloat(campaign.transferred_amount))
+                          ? parseFloat(
+                              campaign.transferred_amount,
+                            ).toLocaleString()
+                          : 0}
+                        <span className="text-gray-600 dark:text-gray-100 truncate">
+                          <span className="text-xs p-1">of</span>
+                          {fundraiserCurrency}
+                          {parseFloat(campaign.goal_amount).toLocaleString()}
+                        </span>
+                      </p>
+                    </div>
                   </div>
+                </Link>
+                <div
+                  className="absolute top-2 right-2 p-2 bg-white dark:bg-gray-700 rounded-full shadow-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-300"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    campaign.favorited
+                      ? handleUnfavorite(campaign.id.toString())
+                      : handleFavorite(campaign.id.toString());
+                  }}
+                >
+                  {campaign.favorited ? (
+                    <FaHeart className="text-orange-500" />
+                  ) : (
+                    <FaRegHeart className="text-gray-700 dark:text-gray-300" />
+                  )}
                 </div>
-              </div>
-              <div className="px-4 py-3 h-full bg-gray-50 dark:bg-gray-800">
-                <div className="w-full text-xs">
-                  <Progress
-                    firstProgress={
-                      (Number(campaign?.transferred_amount) /
-                        Number(campaign?.goal_amount)) *
-                      100
-                    }
-                    firstTooltipContent={`Progress: ${
-                      (Number(campaign?.transferred_amount) /
-                        Number(campaign?.goal_amount)) *
-                      100
-                    }%`}
-                  />
-                </div>
-                <div className="w-full text-xs text-gray-600 dark:text-gray-300 flex flex-col">
-                  <h3 className="text-lg font-bold text-gray-700 dark:text-gray-100 truncate mb-1">
-                    {campaign?.title}
-                  </h3>
-                  <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
-                    {fundraiserCurrency}
-                    {!isNaN(parseFloat(campaign.transferred_amount))
-                      ? parseFloat(
-                          campaign.transferred_amount,
-                        ).toLocaleString()
-                      : 0}
-                    <span className="text-gray-600 dark:text-gray-100 truncate">
-                      <span className="text-xs p-1">of</span>
-                      {fundraiserCurrency}
-                      {parseFloat(campaign.goal_amount).toLocaleString()}
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </Link>
-            <div
-              className="absolute top-2 right-2 p-2 bg-white dark:bg-gray-700 rounded-full shadow-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-300"
-              onClick={(e) => {
-                e.preventDefault();
-                campaign.favorited
-                  ? handleUnfavorite(campaign.id.toString())
-                  : handleFavorite(campaign.id.toString());
-              }}
-            >
-              {campaign.favorited ? (
-                <FaHeart className="text-orange-500" />
-              ) : (
-                <FaRegHeart className="text-gray-700 dark:text-gray-300" />
-              )}
-            </div>
-          </motion.div>
-        );
-      })}
+              </motion.div>
+            );
+          })}
+        </div>
+      )}
     </div>
-  )}
-</div>
   );
 };
 
