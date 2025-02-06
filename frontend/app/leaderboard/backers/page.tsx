a6b8a9e578da8460783d6313a162b95802e89600
josephadeabah <abeansah.lx@gmail.com>
2025-02-06 11:10:07 +0000

    feat: add verified badge

--- frontend/app/leaderboard/backers/page.tsx
+++ frontend/app/leaderboard/backers/page.tsx
@@ -147,7 +147,12 @@ const LeaderboardBackersPage = () => {
                             </div>
                           </PopoverContent>
                         </Popover>
-                        <span className="text-gray-700">{backer.username}</span>
+                        <div className="flex items-center gap-1">
+                          <span className="text-gray-700">
+                            {backer.username}
+                          </span>
+                          <span>{getVerifiedBadge(backer.level)}</span>
+                        </div>
                       </td>
                       <td className="px-4 py-2 text-gray-700">
                         {backer?.currency?.toUpperCase()}{' '}
