63f53b1d3f6721e4e923c414963376dc95baffa8
josephadeabah <abeansah.lx@gmail.com>
2025-02-07 22:02:38 +0000

    feat: add cofund page

--- frontend/app/cofund/page.tsx
+++ frontend/app/cofund/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CoFundPage: React.FC = () => {
   return (
-    <div className="w-full max-w-7xl mx-auto min-h-screen p-4 bg-gray-50 text-gray-900">
+    <div className="w-full max-w-7xl mx-auto min-h-screen bg-gray-50 text-gray-900">
       {/* Hero Section */}
-      <header className="relative bg-blue-300 text-white py-20 px-6 text-center">
+      <header className="relative bg-green-600 text-white py-20 px-6 text-center">
         <h1 className="text-4xl md:text-5xl font-bold">Introducing Co-fund</h1>
         <p className="mt-4 text-lg md:text-xl">
           Co-invest in small and medium-sized businesses for real returns.
