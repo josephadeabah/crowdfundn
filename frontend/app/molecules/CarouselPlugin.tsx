654fdf4f035118843fbfb74c74c353c8bed66511
josephadeabah <abeansah.lx@gmail.com>
2025-02-07 07:17:48 +0000

    chore: change carousel

--- frontend/app/molecules/CarouselPlugin.tsx
+++ frontend/app/molecules/CarouselPlugin.tsx
@@ -1,32 +1,27 @@
 import React from 'react';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
-} from '../components/carousel/Carousel';
-import Autoplay from 'embla-carousel-autoplay';
+import Slider from 'react-slick'; // Import react-slick
 import { Card, CardContent } from '../components/card/Card';
 import Image from 'next/image';
 import data from '../../data.json';
 import { handleContextMenu } from '../utils/helpers/base64.image';
 
-export function CarouselPlugin() {
-  const plugin = React.useRef(
-    Autoplay({ delay: 4000, stopOnInteraction: true }),
-  );
+const CarouselPlugin = () => {
+  const settings = {
+    dots: true, // Show dots for navigation
+    infinite: true, // Infinite scrolling
+    speed: 500, // Speed of slide transition
+    slidesToShow: 1, // Number of slides to show at once
+    slidesToScroll: 1, // Number of slides to scroll at once
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 4000, // Autoplay delay in ms
+    pauseOnHover: true, // Pause autoplay when hovering
+  };
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-full max-w-screen-xl dark:bg-gray-950"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
+    <div className="w-full max-w-screen-xl mx-auto px-4">
+      <Slider {...settings}>
         {data.recommendedFundraisers.map((fundraiser) => (
-          <CarouselItem key={fundraiser.id}>
+          <div key={fundraiser.id}>
             <Card className="w-full rounded-none shadow-none border-0">
               <CardContent className="w-full p-0 flex aspect-square items-center justify-center h-full">
                 <Image
@@ -35,9 +30,8 @@ export function CarouselPlugin() {
                   sizes="100vw"
                   style={{
                     width: '100%',
-                    height: '100%', // Set height to 100% to fill the container
-                    objectFit: 'contain', // This ensures the image maintains its aspect ratio
-                    // borderRadius: '100%'
+                    height: '100%',
+                    objectFit: 'contain',
                   }}
                   width={500}
                   height={300}
@@ -45,11 +39,11 @@ export function CarouselPlugin() {
                 />
               </CardContent>
             </Card>
-          </CarouselItem>
+          </div>
         ))}
-      </CarouselContent>
-      <CarouselPrevious className="left-4 text-white" />
-      <CarouselNext className="right-4 text-white" />
-    </Carousel>
+      </Slider>
+    </div>
   );
-}
+};
+
+export default CarouselPlugin;
