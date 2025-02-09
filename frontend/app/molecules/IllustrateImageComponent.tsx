66484a21a095ebd600429215d0a7788bfe035f96
josephadeabah <abeansah.lx@gmail.com>
2025-02-09 22:17:30 +0000

    chore: reduce width of hr element

--- frontend/app/molecules/IllustrateImageComponent.tsx
+++ frontend/app/molecules/IllustrateImageComponent.tsx
@@ -6,7 +6,7 @@ interface IllustrateImageComponentProps {
 
 const IllustrateImageComponent: React.FC<IllustrateImageComponentProps> = ({ images }) => {
   return (
-    <div className="w-full bg-[#fafaeb] py-10">
+    <div className="w-full bg-green-600 py-10">
       <div className="max-w-7xl mx-auto flex flex-wrap justify-center gap-6">
         {images.map((src, index) => (
           <img key={index} src={src} alt={`Illustration ${index + 1}`} className="max-w-sm w-full object-cover" />
