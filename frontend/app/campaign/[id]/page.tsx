90208dda96400a0472c003a5904047f595296f16
josephadeabah <abeansah.lx@gmail.com>
2025-02-12 09:43:52 +0000

    chore: redesign single page

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -172,7 +172,7 @@ const SingleCampaignPage: React.FC = () => {
                 </button>
                 <div
                   ref={tabsRef}
-                  className="max-w-7xl mx-auto flex space-x-4 overflow-x-auto scrollbar-hide whitespace-nowrap"
+                  className="flex overflow-x-auto scrollbar-hide whitespace-nowrap"
                 >
                   {['details', 'donate', 'updates', 'comments', 'backers'].map(
                     (tab) => {
