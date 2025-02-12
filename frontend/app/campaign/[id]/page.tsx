1dbaa70f10f25efcaf02d8cb814d6864159bdd3f
josephadeabah <abeansah.lx@gmail.com>
2025-02-12 10:19:33 +0000

    chore: redesign single page

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -137,7 +137,7 @@ const SingleCampaignPage: React.FC = () => {
   if (loading) return <SingleCampaignLoader />;
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mb-12">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 py-8 mb-12">
       <div className="flex flex-col lg:flex-row gap-8 mb-10">
         {/* First Column (Bigger Width) */}
         <div className="lg:w-2/3">
