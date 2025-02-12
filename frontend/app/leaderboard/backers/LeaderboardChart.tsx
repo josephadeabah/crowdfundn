f404be752d38379c1a960dbc9f0f998fa5533d7c
josephadeabah <abeansah.lx@gmail.com>
2025-02-12 16:58:39 +0000

    feat: add charts to backer page

--- frontend/app/leaderboard/backers/LeaderboardChart.tsx
+++ frontend/app/leaderboard/backers/LeaderboardChart.tsx
@@ -46,15 +46,15 @@ const CustomizedDollarDot = (props: {
 
   return (
     <svg
-    x={cx - 10}
-    y={cy - 10}
-    width={20}
-    height={20}
-    fill="#C14600" // Green color for dollar sign
-    viewBox="0 0 24 24"
-  >
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
-  </svg>
+      x={cx - 10}
+      y={cy - 10}
+      width={20}
+      height={20}
+      fill="#C14600" // Green color for dollar sign
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
+    </svg>
   );
 };
 
