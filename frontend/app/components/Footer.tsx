d3be80739fb32b749136fe7fc24e0ccd8cff94c1
josephadeabah <abeansah.lx@gmail.com>
2025-02-13 08:17:32 +0000

    fix: redesign footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -4,9 +4,9 @@ import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="relative bg-gradient-to-r from-green-500 to-green-800 hover:from-green-800 text-gray-50 py-16">
+    <footer className="relative bg-gradient-to-r from-green-500 to-green-800 hover:from-green-800 hover:to-green-700 text-gray-50 py-16">
       {/* Background Image */}
-      <div className="absolute inset-0  bg-contain opacity-10" style={{ backgroundImage: "url('/Team-spirit-bro.svg')" }}></div>
+      <div className="absolute inset-0 bg-no-repeat bg-contain opacity-10" style={{ backgroundImage: "url('/Team-spirit-bro.svg')" }}></div>
 
       <div className="relative container mx-auto px-6 md:px-12 lg:px-20">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
