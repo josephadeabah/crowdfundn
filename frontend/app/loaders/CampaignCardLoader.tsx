f49e7e6b0184afafb92144cfe8d7c6d8d4b6b7d3
josephadeabah <abeansah.lx@gmail.com>
2025-02-14 10:44:48 +0000

    fix: remove top graphics

--- frontend/app/loaders/CampaignCardLoader.tsx
+++ frontend/app/loaders/CampaignCardLoader.tsx
@@ -4,7 +4,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const CampaignCardLoader = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 px-3 md:p-6">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 px-3">
       {Array(10)
         .fill(null)
         .map((_, index) => (
