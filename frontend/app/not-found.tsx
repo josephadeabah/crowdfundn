4fd464a5b81541252e614fb4b247a38a050e1124
josephadeabah <abeansah.lx@gmail.com>
2025-02-15 18:51:43 +0000

    fix: image build

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
+      <h1 className="text-5xl font-bold text-red-600">404 - Page Not Found</h1>
       <p className="mt-2 text-lg text-gray-600">
         Sorry, the page you are looking for does not exist yet.
       </p>
