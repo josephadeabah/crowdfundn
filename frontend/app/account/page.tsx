4f0bf6cd2cf6e80378eb1b9233af63b0bc89a29c
josephadeabah <abeansah.lx@gmail.com>
2025-02-16 05:36:02 +0000

    feat: add more metrics and charts to dashboard

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -133,67 +133,69 @@ const ProfileTabs = () => {
   }
 
   return (
-    <div className="w-full bg-white dark:bg-gray-800 flex flex-col md:flex-row h-screen">
-      {/* Tabs Menu */}
-      <div className="md:w-1/6 border-b md:border-b-0 md:border-r-2 border-dashed border-orange-200 dark:border-neutral-700">
-        <div
-          className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 !overflow-x-auto md:overflow-visible"
-          aria-label="Tabs"
-        >
-          {tabs.map(({ label, icon }, index) => {
-            const isActive = activeTab === label;
-            const isOnboarding = showOnboarding && currentStep === index;
+    <div className="w-full bg-white dark:bg-gray-800">
+      <div className="max-w-7xl mx-auto flex flex-col mt-0 md:flex-row h-screen">
+        {/* Tabs Menu */}
+        <div className="md:w-1/6 border-b h-auto md:h-screen md:border-b-0 md:border-r-2 border-dashed border-orange-200 dark:border-neutral-700">
+          <div
+            className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 !overflow-x-auto md:overflow-visible"
+            aria-label="Tabs"
+          >
+            {tabs.map(({ label, icon }, index) => {
+              const isActive = activeTab === label;
+              const isOnboarding = showOnboarding && currentStep === index;
 
-            return (
-              <a
-                key={label}
-                href={`#${label}`} // Anchor link
-                className={`py-3 px-3 h-full whitespace-nowrap text-sm font-medium md:text-base transform transition-transform duration-300 ${
-                  isActive
-                    ? 'border-b-2 border-2 border-dashed md:border-b-0 md:border-l-2 md:border-r-0 border-orange-200 text-orange-400 dark:text-orange-600'
-                    : 'border-transparent text-gray-500 hover:bg-gray-100 dark:hover:bg-neutral-700 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-gray-950'
-                } flex items-center focus:outline-none ${
-                  isOnboarding
-                    ? 'bg-green-600 text-white dark:bg-orange-700'
-                    : ''
-                }`}
-                onClick={(e) => {
-                  e.preventDefault(); // Prevent default anchor behavior
-                  handleTabClick(label); // Handle tab click
-                }}
-                aria-selected={isActive}
-                aria-controls={`vertical-tab-${label}`}
-                role="tab"
-                id={`tab-${label}`} // Specific ID for better targeting
-              >
-                <span className="mr-2">{icon}</span>
-                {label}
-              </a>
-            );
-          })}
+              return (
+                <a
+                  key={label}
+                  href={`#${label}`} // Anchor link
+                  className={`py-3 px-3 h-full whitespace-nowrap text-sm font-medium md:text-base transform transition-transform duration-300 ${
+                    isActive
+                      ? 'border-b-2 border-2 border-dashed md:border-b-0 md:border-l-2 md:border-r-0 border-orange-200 text-orange-400 dark:text-orange-600'
+                      : 'border-transparent text-gray-500 hover:bg-gray-100 dark:hover:bg-neutral-700 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-gray-950'
+                  } flex items-center focus:outline-none ${
+                    isOnboarding
+                      ? 'bg-green-600 text-white dark:bg-orange-700'
+                      : ''
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault(); // Prevent default anchor behavior
+                    handleTabClick(label); // Handle tab click
+                  }}
+                  aria-selected={isActive}
+                  aria-controls={`vertical-tab-${label}`}
+                  role="tab"
+                  id={`tab-${label}`} // Specific ID for better targeting
+                >
+                  <span className="mr-2">{icon}</span>
+                  {label}
+                </a>
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      {/* Tab Content */}
-      <div className="flex-1 bg-gradient-to-tr from-green-50 to-orange-50 dark:from-green-900 dark:to-orange-900 dark:bg-gray-900 px-4 py-4 mb-20 h-full overflow-y-auto  [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
-        <div
-          role="tabpanel"
-          id={`vertical-tab-${activeTab}`}
-          className="h-full"
-        >
-          {tabs.find((tab) => tab.label === activeTab)?.component}
+        {/* Tab Content */}
+        <div className="flex-1 bg-gradient-to-tr from-green-50 to-orange-50 dark:from-green-900 dark:to-orange-900 dark:bg-gray-900 px-4 py-4 overflow-auto h-full md:h-screen [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
+          <div
+            role="tabpanel"
+            id={`vertical-tab-${activeTab}`}
+            className="h-full"
+          >
+            {tabs.find((tab) => tab.label === activeTab)?.component}
+          </div>
         </div>
-      </div>
 
-      {/* Onboarding Modal */}
-      {showOnboarding && (
-        <OnboardingModal
-          currentStep={currentStep}
-          setCurrentStep={setCurrentStep}
-          completeOnboarding={completeOnboarding}
-          tabs={tabs}
-        />
-      )}
+        {/* Onboarding Modal */}
+        {showOnboarding && (
+          <OnboardingModal
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+            completeOnboarding={completeOnboarding}
+            tabs={tabs}
+          />
+        )}
+      </div>
     </div>
   );
 };
