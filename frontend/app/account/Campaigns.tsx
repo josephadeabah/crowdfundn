d46c3f81cfce7a18db3220f2b12ffbf0f18df127
josephadeabah <abeansah.lx@gmail.com>
2025-02-16 16:59:47 +0000

    feat: add notification for public or private campaign

--- frontend/app/account/Campaigns.tsx
+++ frontend/app/account/Campaigns.tsx
@@ -156,10 +156,10 @@ const Campaigns: React.FC = () => {
                   </Button>
                   <div className="flex items-center gap-1">
                     <span
-                      className={`w-3 h-3 rounded-full 
+                      className={`w-2 h-2 rounded-full 
         ${campaign.permissions.is_public ? 'bg-green-500' : 'bg-gray-500'}`}
                     ></span>
-                    <span className="text-gray-500 font-semibold text-sm">
+                    <span className="text-gray-500 font-semibold text-xs">
                       {campaign.permissions.is_public ? 'Public' : 'Private'}
                     </span>
                   </div>
