ee13fb67cf6116bc94f0a1fecabe5b244f3250aa
josephadeabah <abeansah.lx@gmail.com>
2025-02-17 18:50:40 +0000

    fix: alignment on backer table

--- frontend/app/leaderboard/backers/page.tsx
+++ frontend/app/leaderboard/backers/page.tsx
@@ -86,7 +86,7 @@ const LeaderboardBackersPage = () => {
                       <td className="px-4 py-2 text-gray-600">
                         {getRankWithSuffix(backer.rank)}
                       </td>
-                      <td className="px-4 py-2 flex items-center">
+                      <td className="px-4 py-2 flex items-center space-x-3">
                         <Popover>
                           <PopoverTrigger asChild>
                             <div className="relative cursor-pointer">
