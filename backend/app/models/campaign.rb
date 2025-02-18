39bf52673c91b0b5cffd15abcd03fc2ea26208b2
josephadeabah <abeansah.lx@gmail.com>
2025-02-18 02:11:30 +0000

    fix: update is public when campaign complete

--- backend/app/models/campaign.rb
+++ backend/app/models/campaign.rb
@@ -125,7 +125,7 @@ class Campaign < ApplicationRecord
 
   def update_status_based_on_date
     return if canceled? # Skip if already canceled
-    update!(status: :completed)
+    update!(status: :completed, is_public: false)
   end  
 
   # Calculate the total number of unique donors (authenticated + anonymous)
