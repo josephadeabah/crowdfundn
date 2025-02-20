32c3117829736cf09455602a102caa4eb341b10e
josephadeabah <abeansah.lx@gmail.com>
2025-02-20 10:57:47 +0000

    fix: add campaign button added to campaigns tab

--- frontend/app/loaders/CampaignsLoader.tsx
+++ frontend/app/loaders/CampaignsLoader.tsx
@@ -6,7 +6,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 const CampaignsLoader = () => {
   return (
     <div className="max-w-7xl mx-auto px-2 py-8">
-      <div className="flex justify-between items-center mb-8">
+      <div className="flex justify-between items-center">
         <h1 className="text-3xl font-bold text-gray-800">Campaigns</h1>
         <button className="flex items-center px-4 py-2 bg-gray-300 text-white rounded-lg">
           <FiPlus className="mr-2" />
