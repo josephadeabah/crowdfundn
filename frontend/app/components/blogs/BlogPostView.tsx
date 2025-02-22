3f247f22f215e727fea9842c16ea94c239970c03
josephadeabah <abeansah.lx@gmail.com>
2025-02-22 15:55:20 +0000

    fix: blog slug

--- frontend/app/components/blogs/BlogPostView.tsx
+++ frontend/app/components/blogs/BlogPostView.tsx
@@ -23,32 +23,40 @@ const BlogPostView: React.FC = () => {
   if (!currentArticle) return <p className="text-center">Article not found</p>;
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-8">
-      <h1 className="text-6xl font-bold text-center mb-6">
-        {currentArticle.title}
-      </h1>
+    <div className="flex flex-col items-center">
+      {/* Title & Featured Image (max-w-7xl) */}
+      <div className="max-w-7xl w-full px-2 py-8">
+        <h1 className="text-6xl font-bold text-center mb-6">
+          {currentArticle.title}
+        </h1>
 
-      {currentArticle.featured_image && (
-        <div className="relative w-full h-64 md:h-96">
-          <Image
-            src={currentArticle.featured_image}
-            alt={currentArticle.title}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-lg"
+        {currentArticle.featured_image && (
+          <div className="relative w-full h-64 md:h-96">
+            <Image
+              src={currentArticle.featured_image}
+              alt={currentArticle.title}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            />
+          </div>
+        )}
+      </div>
+
+      {/* Rest of the Content (max-w-4xl) */}
+      <div className="max-w-4xl w-full px-4 py-8">
+        <p className="text-gray-500 text-sm mt-4 text-center">
+          Published on{' '}
+          {moment(currentArticle.published_at).format('MMMM Do, YYYY')}
+        </p>
+
+        <div className="prose dark:prose-dark max-w-none mt-6 text-lg text-gray-800">
+          <p
+            dangerouslySetInnerHTML={{
+              __html: currentArticle.description.body,
+            }}
           />
         </div>
-      )}
-
-      <p className="text-gray-500 text-sm mt-4 text-center">
-        Published on{' '}
-        {moment(currentArticle.published_at).format('MMMM Do, YYYY')}
-      </p>
-
-      <div className="prose dark:prose-dark max-w-none mt-6 text-lg text-gray-800">
-        <p
-          dangerouslySetInnerHTML={{ __html: currentArticle.description.body }}
-        />
       </div>
     </div>
   );
