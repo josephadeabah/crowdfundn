b803b6813e19c9804afa67a9a3443bf6cce8baf0
josephadeabah <abeansah.lx@gmail.com>
2025-02-22 12:01:20 +0000

    fix: seperate description field

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_22_070044) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_22_104829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_22_070044) do
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
