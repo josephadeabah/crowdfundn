fe2bebd8ca2c61c9f81558b44552fd47f49d438c
josephadeabah <abeansah.lx@gmail.com>
2025-02-22 16:56:47 +0000

    fix: add proper loaders for articles

--- frontend/app/components/blogs/BlogPosts.tsx
+++ frontend/app/components/blogs/BlogPosts.tsx
@@ -21,33 +21,37 @@ const BlogPosts: React.FC = () => {
     <div className="max-w-7xl mx-auto px-2 py-8">
       <h2 className="text-3xl font-bold text-center mb-8">Latest Blog Posts</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {articles.map((article) => (
-          <Link
-            key={article.id}
-            href={`/blog/${article.slug}`} // Make the whole card clickable
-            className="block bg-white rounded-lg overflow-hidden"
-          >
-            {article.featured_image && (
-              <div className="relative w-full h-48">
-                <Image
-                  src={article.featured_image}
-                  alt={article.title}
-                  layout="fill"
-                  objectFit="cover"
-                  priority
-                />
+        {articles.slice(0, 3).map(
+          (
+            article, // Limit to 3 articles
+          ) => (
+            <Link
+              key={article.id}
+              href={`/blog/${article.slug}`} // Make the whole card clickable
+              className="block bg-white rounded-lg overflow-hidden"
+            >
+              {article.featured_image && (
+                <div className="relative w-full h-48">
+                  <Image
+                    src={article.featured_image}
+                    alt={article.title}
+                    layout="fill"
+                    objectFit="cover"
+                    priority
+                  />
+                </div>
+              )}
+              <div className="p-6">
+                <h3 className="text-xl font-semibold mb-2">{article.title}</h3>
+                <p className="text-gray-700">{article.meta_description}</p>
+                <p className="text-gray-500 text-sm mt-4">
+                  Published on{' '}
+                  {moment(article.published_at).format('MMMM Do, YYYY')}
+                </p>
               </div>
-            )}
-            <div className="p-6">
-              <h3 className="text-xl font-semibold mb-2">{article.title}</h3>
-              <p className="text-gray-700">{article.meta_description}</p>
-              <p className="text-gray-500 text-sm mt-4">
-                Published on{' '}
-                {moment(article.published_at).format('MMMM Do, YYYY')}
-              </p>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ),
+        )}
       </div>
     </div>
   );
