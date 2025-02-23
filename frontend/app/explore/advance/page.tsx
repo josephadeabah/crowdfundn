909ca2e1c53f30365c71ad7489e1d6d540b94150
josephadeabah <abeansah.lx@gmail.com>
2025-02-23 23:11:05 +0000

    fix: add theme provider for material tailwind css

--- frontend/app/explore/advance/page.tsx
+++ frontend/app/explore/advance/page.tsx
@@ -185,6 +185,13 @@ const CampaignsPage = () => {
     await unfavoriteCampaign(campaignId);
   };
 
+  // Calculate the campaigns to display on the current page
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const displayedCampaigns = campaigns
+    .filter((campaign) => campaign?.permissions?.is_public)
+    .slice(startIndex, endIndex);
+
   return (
     <div className="max-w-7xl mx-auto p-4">
       <ToastComponent
@@ -363,149 +370,139 @@ const CampaignsPage = () => {
             <CampaignCardLoader />
           ) : (
             <div>
-              {campaigns && campaigns.length > 0 ? (
+              {displayedCampaigns.length > 0 ? (
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 p-3 relative">
-                  {campaigns
-                    .filter((campaign) => campaign?.permissions?.is_public)
-                    .slice(
-                      0,
-                      window.innerWidth >= 1024
-                        ? 10 // Large screens (5x2)
-                        : window.innerWidth >= 768
-                          ? 10 // Tablet screens (3x4)
-                          : 10, // Mobile screens (2x6)
-                    )
-                    .map((campaign, index) => {
-                      const fundraiserCurrency =
-                        campaign?.currency_symbol ||
-                        campaign?.currency?.toUpperCase();
+                  {displayedCampaigns.map((campaign, index) => {
+                    const fundraiserCurrency =
+                      campaign?.currency_symbol ||
+                      campaign?.currency?.toUpperCase();
 
-                      return (
-                        <motion.div
-                          key={campaign?.id}
-                          initial="hidden"
-                          animate="visible"
-                          exit="hidden"
-                          transition={{ duration: 0.5, delay: index * 0.1 }}
-                          className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 hover:shadow-lg transition-transform duration-300 cursor-pointer overflow-hidden"
+                    return (
+                      <motion.div
+                        key={campaign?.id}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                        transition={{ duration: 0.5, delay: index * 0.1 }}
+                        className="group relative bg-white dark:bg-gray-900 flex flex-col h-full dark:text-gray-50 hover:shadow-lg transition-transform duration-300 cursor-pointer overflow-hidden"
+                      >
+                        <Link
+                          href={`/campaign/${campaign?.id}?${generateRandomString()}`}
                         >
-                          <Link
-                            href={`/campaign/${campaign?.id}?${generateRandomString()}`}
-                          >
-                            <Card className="h-full">
-                              <div className="relative w-full h-0 pb-[100%]">
-                                <Image
-                                  src={campaign?.media || '/bantuhive.svg'}
-                                  alt="media thumbnail"
-                                  layout="fill"
-                                  objectFit="cover"
-                                  className="absolute top-0 left-0 w-full h-full"
+                          <Card className="h-full">
+                            <div className="relative w-full h-0 pb-[100%]">
+                              <Image
+                                src={campaign?.media || '/bantuhive.svg'}
+                                alt="media thumbnail"
+                                layout="fill"
+                                objectFit="cover"
+                                className="absolute top-0 left-0 w-full h-full"
+                              />
+                            </div>
+                            <div className="px-2 py-2 bg-gray-50 dark:text-gray-50">
+                              <div className="w-full text-xs">
+                                <Progress
+                                  firstProgress={
+                                    (Number(campaign?.transferred_amount) /
+                                      Number(campaign?.goal_amount)) *
+                                    100
+                                  }
+                                  firstTooltipContent={`Progress: ${
+                                    (Number(campaign?.transferred_amount) /
+                                      Number(campaign?.goal_amount)) *
+                                    100
+                                  }%`}
                                 />
                               </div>
-                              <div className="px-2 py-2 bg-gray-50 dark:text-gray-50">
-                                <div className="w-full text-xs">
-                                  <Progress
-                                    firstProgress={
-                                      (Number(campaign?.transferred_amount) /
-                                        Number(campaign?.goal_amount)) *
-                                      100
-                                    }
-                                    firstTooltipContent={`Progress: ${
-                                      (Number(campaign?.transferred_amount) /
-                                        Number(campaign?.goal_amount)) *
-                                      100
-                                    }%`}
-                                  />
-                                </div>
-                                <div className="w-full text-xs text-gray-600 flex flex-col py-2">
-                                  <div className="flex justify-between items-center mb-2">
-                                    <div className="flex items-center space-x-2">
-                                      <Avatar
-                                        name={campaign?.fundraiser?.profile?.name}
-                                        size="sm"
-                                        imageUrl={
-                                          campaign?.fundraiser?.profile?.avatar
-                                        }
-                                      />
-                                      <span className="text-sm font-semibold w-24 truncate">
-                                        {campaign?.fundraiser?.profile?.name}
-                                      </span>
-                                    </div>
-                                    <div
-                                      className="p-2 bg-white dark:bg-gray-700 rounded-full shadow-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-300"
-                                      onClick={(e) => {
-                                        e.preventDefault();
-                                        campaign?.favorited
-                                          ? handleUnfavorite(
-                                              campaign?.id.toString(),
-                                            )
-                                          : handleFavorite(
-                                              campaign?.id.toString(),
-                                            );
-                                      }}
-                                    >
-                                      {campaign.favorited ? (
-                                        <FaHeart className="text-orange-500" />
-                                      ) : (
-                                        <FaRegHeart className="text-gray-700 dark:text-gray-300" />
-                                      )}
-                                    </div>
+                              <div className="w-full text-xs text-gray-600 flex flex-col py-2">
+                                <div className="flex justify-between items-center mb-2">
+                                  <div className="flex items-center space-x-2">
+                                    <Avatar
+                                      name={campaign?.fundraiser?.profile?.name}
+                                      size="sm"
+                                      imageUrl={
+                                        campaign?.fundraiser?.profile?.avatar
+                                      }
+                                    />
+                                    <span className="text-sm font-semibold w-24 truncate">
+                                      {campaign?.fundraiser?.profile?.name}
+                                    </span>
                                   </div>
-                                  <h3 className="text-lg font-bold text-gray-700 truncate mb-1">
-                                    {campaign?.title}
-                                  </h3>
-                                  <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
-                                    <span
-                                      className={`${
-                                        parseFloat(
-                                          campaign?.transferred_amount?.toString() ||
-                                            '0',
-                                        ) >=
-                                        parseFloat(
-                                          campaign?.goal_amount?.toString() ||
-                                            '0',
-                                        )
-                                          ? 'text-green-600'
-                                          : 'text-orange-500'
-                                      }`}
-                                    >
-                                      <span className="text-gray-600 dark:text-gray-100 mr-1">
-                                        {fundraiserCurrency}
-                                      </span>
-                                      {parseFloat(
+                                  <div
+                                    className="p-2 bg-white dark:bg-gray-700 rounded-full shadow-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-300"
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      campaign?.favorited
+                                        ? handleUnfavorite(
+                                            campaign?.id.toString(),
+                                          )
+                                        : handleFavorite(
+                                            campaign?.id.toString(),
+                                          );
+                                    }}
+                                  >
+                                    {campaign.favorited ? (
+                                      <FaHeart className="text-orange-500" />
+                                    ) : (
+                                      <FaRegHeart className="text-gray-700 dark:text-gray-300" />
+                                    )}
+                                  </div>
+                                </div>
+                                <h3 className="text-lg font-bold text-gray-700 truncate mb-1">
+                                  {campaign?.title}
+                                </h3>
+                                <p className="flex justify-between items-center text-sm font-semibold mt-2 break-words">
+                                  <span
+                                    className={`${
+                                      parseFloat(
                                         campaign?.transferred_amount?.toString() ||
                                           '0',
-                                      ).toLocaleString()}
-                                    </span>{' '}
-                                    <span className="text-gray-600 dark:text-gray-100 truncate">
-                                      <span className="text-xs p-1">of</span>
+                                      ) >=
+                                      parseFloat(
+                                        campaign?.goal_amount?.toString() ||
+                                          '0',
+                                      )
+                                        ? 'text-green-600'
+                                        : 'text-orange-500'
+                                    }`}
+                                  >
+                                    <span className="text-gray-600 dark:text-gray-100 mr-1">
                                       {fundraiserCurrency}
-                                      {parseFloat(
-                                        campaign?.goal_amount,
-                                      ).toLocaleString()}
                                     </span>
-                                  </p>
-                                  <div className="block md:flex justify-between items-center text-xs font-semibold text-gray-500 dark:text-gray-400 mt-2">
-                                    <div className="flex items-center space-x-1">
-                                      <FaUser />
-                                      <span>
-                                        {campaign?.total_donors || 0} Backers
-                                      </span>
-                                    </div>
-                                    <div className="flex items-center space-x-1">
-                                      <FaClock />
-                                      <span>
-                                        {campaign?.remaining_days} days left
-                                      </span>
-                                    </div>
+                                    {parseFloat(
+                                      campaign?.transferred_amount?.toString() ||
+                                        '0',
+                                    ).toLocaleString()}
+                                  </span>{' '}
+                                  <span className="text-gray-600 dark:text-gray-100 truncate">
+                                    <span className="text-xs p-1">of</span>
+                                    {fundraiserCurrency}
+                                    {parseFloat(
+                                      campaign?.goal_amount,
+                                    ).toLocaleString()}
+                                  </span>
+                                </p>
+                                <div className="block md:flex justify-between items-center text-xs font-semibold text-gray-500 dark:text-gray-400 mt-2">
+                                  <div className="flex items-center space-x-1">
+                                    <FaUser />
+                                    <span>
+                                      {campaign?.total_donors || 0} Backers
+                                    </span>
+                                  </div>
+                                  <div className="flex items-center space-x-1">
+                                    <FaClock />
+                                    <span>
+                                      {campaign?.remaining_days} days left
+                                    </span>
                                   </div>
                                 </div>
                               </div>
-                            </Card>
-                          </Link>
-                        </motion.div>
-                      );
-                    })}
+                            </div>
+                          </Card>
+                        </Link>
+                      </motion.div>
+                    );
+                  })}
                 </div>
               ) : (
                 <p className="text-center text-gray-500">
