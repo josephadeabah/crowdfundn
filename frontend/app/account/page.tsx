a5b437ddfabf67f57e24af8321c12b6473fa8656
josephadeabah <abeansah.lx@gmail.com>
2025-02-25 15:46:26 +0000

    fix: revert prev

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -12,13 +12,6 @@ import {
   StarIcon,
 } from '@radix-ui/react-icons';
 import { BiTransfer } from 'react-icons/bi';
-import {
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-} from '@material-tailwind/react';
 import Rewards from '@/app/account/Rewards';
 import Campaigns from '@/app/account/Campaigns';
 import Transfers from '@/app/account/Transfers';
@@ -31,60 +24,60 @@ import OnboardingModal from '@/app/components/onboarding/OnboardingModal';
 import Favorites from '@/app/account/Favorites';
 
 const ProfileTabs = () => {
-  const [activeTab, setActiveTab] = useState<string>('Dashboard');
+  const [activeTab, setActiveTab] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
   const [showOnboarding, setShowOnboarding] = useState<boolean>(false);
-  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [currentStep, setCurrentStep] = useState<number>(0); // To track the onboarding step
 
-  // Tab titles, icons, and components
+  // Tab titles and icons
   const tabs = [
     {
       label: 'Dashboard',
-      icon: <DashboardIcon className="h-5 w-5" />,
+      icon: <DashboardIcon />,
       component: <Dashboard />,
       description:
         'Your overall dashboard where you can see an overview of activities and analytics.',
     },
     {
       label: 'Donations',
-      icon: <HandIcon className="h-5 w-5" />,
+      icon: <HandIcon />,
       component: <Donations />,
       description:
         'Manage your donations and send thank you to your backers here.',
     },
     {
       label: 'Transfers',
-      icon: <BiTransfer className="h-5 w-5" />,
+      icon: <BiTransfer />,
       component: <Transfers />,
       description: 'View and manage your transfers.',
     },
     {
       label: 'Rewards',
-      icon: <IconJarLogoIcon className="h-5 w-5" />,
+      icon: <IconJarLogoIcon />,
       component: <Rewards />,
       description: 'Receive and give rewards to your donors here.',
     },
     {
       label: 'Campaigns',
-      icon: <RocketIcon className="h-5 w-5" />,
+      icon: <RocketIcon />,
       component: <Campaigns />,
       description: 'Create and manage your fundraising campaigns.',
     },
     {
       label: 'Updates',
-      icon: <ChatBubbleIcon className="h-5 w-5" />,
+      icon: <ChatBubbleIcon />,
       component: <CampaignUpdates />,
       description: 'Add your fundraising updates in this tab.',
     },
     {
-      label: 'Favorites',
-      icon: <StarIcon className="h-5 w-5" />,
+      label: 'Favorites', // New Favorites tab
+      icon: <StarIcon />, // Use an appropriate icon
       component: <Favorites />,
       description: 'View your favorited campaigns here.',
     },
     {
       label: 'Settings',
-      icon: <GearIcon className="h-5 w-5" />,
+      icon: <GearIcon />,
       component: <AccountSettings />,
       description:
         'Manage your account and Payment settings here. <p class="text-white bg-red-500 text-xs px-1">IMPORTANT: Please add your bank or mobile money account in this tab after you click Finish or Skip button.</p>',
@@ -120,6 +113,15 @@ const ProfileTabs = () => {
     }
   }, [activeTab]);
 
+  // Handle tab click and loading state
+  const handleTabClick = (tab: string) => {
+    setLoading(true);
+    setActiveTab(tab);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500); // Simulate loading
+  };
+
   // Mark onboarding as completed
   const completeOnboarding = () => {
     localStorage.setItem('onboardingCompleted', 'true');
@@ -135,34 +137,53 @@ const ProfileTabs = () => {
       <div className="max-w-7xl mx-auto flex flex-col mt-0 md:flex-row h-screen">
         {/* Tabs Menu */}
         <div className="md:w-1/6 border-b h-auto md:h-screen md:border-b-0 md:border-r-2 border-dashed border-orange-200 dark:border-neutral-700">
-          <Tabs value={activeTab} orientation="vertical">
-            <TabsHeader className="bg-transparent dark:bg-gray-800">
-              {tabs.map(({ label, icon }) => (
-                <Tab
+          <div
+            className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 !overflow-x-auto md:overflow-visible"
+            aria-label="Tabs"
+          >
+            {tabs.map(({ label, icon }, index) => {
+              const isActive = activeTab === label;
+              const isOnboarding = showOnboarding && currentStep === index;
+
+              return (
+                <a
                   key={label}
-                  value={label}
-                  onClick={() => setActiveTab(label)}
-                  className="flex items-center gap-2 text-left p-3"
+                  href={`#${label}`} // Anchor link
+                  className={`py-3 px-3 h-full whitespace-nowrap text-sm font-medium md:text-base transform transition-transform duration-300 ${
+                    isActive
+                      ? 'border-b-2 border-2 border-dashed md:border-b-0 md:border-l-2 md:border-r-0 border-orange-200 text-orange-400 dark:text-orange-600'
+                      : 'border-transparent text-gray-600 hover:bg-gray-100 dark:hover:bg-neutral-700 hover:text-gray-900 dark:text-neutral-400 dark:hover:text-gray-950'
+                  } flex items-center focus:outline-none ${
+                    isOnboarding
+                      ? 'bg-green-600 text-white dark:bg-orange-700'
+                      : ''
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault(); // Prevent default anchor behavior
+                    handleTabClick(label); // Handle tab click
+                  }}
+                  aria-selected={isActive}
+                  aria-controls={`vertical-tab-${label}`}
+                  role="tab"
+                  id={`tab-${label}`} // Specific ID for better targeting
                 >
-                  {icon}
+                  <span className="mr-2">{icon}</span>
                   {label}
-                </Tab>
-              ))}
-            </TabsHeader>
-          </Tabs>
+                </a>
+              );
+            })}
+          </div>
         </div>
 
         {/* Tab Content */}
-        <div className="flex-1 flex flex-col bg-gradient-to-tr from-green-50 to-orange-50 dark:from-green-900 dark:to-orange-900 dark:bg-gray-900 px-3 mb-0 overflow-auto h-full md:h-screen">
-          <Tabs value={activeTab} orientation="vertical">
-            <TabsBody>
-              {tabs.map(({ label, component }) => (
-                <TabPanel key={label} value={label} className="py-0">
-                  {component}
-                </TabPanel>
-              ))}
-            </TabsBody>
-          </Tabs>
+        <div className="flex-1 flex flex-col bg-gradient-to-tr from-green-50 to-orange-50 dark:from-green-900 dark:to-orange-900 dark:bg-gray-900 px-3 mb-0 overflow-auto h-full md:h-screen [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
+          <div
+            role="tabpanel"
+            id={`vertical-tab-${activeTab}`}
+            className="flex-1 mb-8"
+          >
+            {tabs.find((tab) => tab.label === activeTab)?.component}
+          </div>
           <div className="bg-white w-full m-0 text-center py-4 text-gray-600 dark:text-gray-400">
             © 2025 Bantu Hive Ltd
           </div>
