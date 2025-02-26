24c3b6d13a7a7c12d6a4642b7f2470a4baf800d6
josephadeabah <abeansah.lx@gmail.com>
2025-02-26 22:34:51 +0000

    fix: remove material tailwind react theme

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -297,7 +297,7 @@ const Navbar = () => {
                         passHref
                         className="focus-visible:outline-none focus:ring-0 hover:outline-none"
                       >
-                        <div className="flex items-center gap-3 p-2 hover:bg-gray-200 dark:hover:bg-gray-800 focus-visible:outline-none focus:ring-0 hover:outline-none">
+                        <div className="w-full flex items-center gap-3 p-2 hover:bg-gray-200 dark:hover:bg-gray-800 focus-visible:outline-none focus:ring-0 hover:outline-none">
                           <div className="flex items-center justify-center rounded-lg !bg-white p-2">
                             {React.createElement(link.icon, {
                               className: 'h-5 w-5 text-gray-800',
