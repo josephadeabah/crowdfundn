11dda5a4e95ccfeaa694c40870e633b1d3fe0412
josephadeabah <abeansah.lx@gmail.com>
2025-02-26 21:39:12 +0000

    fix: remove material tailwind react theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,3 @@
-import withMT from '@material-tailwind/react/utils/withMT';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
