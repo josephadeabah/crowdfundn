43b5e534f70f609d9b06926534b3405572ca6dcd
josephadeabah <abeansah.lx@gmail.com>
2025-02-26 18:44:50 +0000

    fix: replace single campaign page tabs for material tailwind

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -225,7 +225,7 @@ const SingleCampaignPage: React.FC = () => {
                   | 'backers',
               ) => setSelectedTab(value)}
             >
-              <TabsHeader>
+              <TabsHeader className='rounded-none'>
                 {tabData.map(({ label, value, count, disabled }) => (
                   <Tab
                     key={value}
