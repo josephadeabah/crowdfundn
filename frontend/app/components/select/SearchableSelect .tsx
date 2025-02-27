5a400ffa895ad3c923d6f1c7f4526eb9a1b0e614
josephadeabah <abeansah.lx@gmail.com>
2025-02-27 11:31:10 +0000

    chore: change colors

--- frontend/app/components/select/SearchableSelect .tsx
+++ frontend/app/components/select/SearchableSelect .tsx
@@ -13,6 +13,7 @@ type SelectComponentProps = {
   label?: string;
   className?: string; // Additional class for further customization
   variant?: 'default' | 'black' | 'outline' | 'ghost' | 'destructive'; // Variant for different styles
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'; // Size for the select box
 };
 
 const SelectComponent: React.FC<SelectComponentProps> = ({
@@ -23,10 +24,20 @@ const SelectComponent: React.FC<SelectComponentProps> = ({
   label,
   className = '',
   variant = 'default',
+  size = 'md', // Default size is 'md'
 }) => {
   // Define base styles for the select box
   const baseStyles =
-    'w-full px-4 py-2 rounded-full text-gray-800 border focus:outline-none focus:ring-2 transition duration-300 ease-in-out appearance-none';
+    'w-full rounded-full text-gray-800 border focus:outline-none focus:ring-2 transition duration-300 ease-in-out appearance-none';
+
+  // Define size styles
+  const sizeStyles = {
+    xs: 'px-2 py-1 text-xs',
+    sm: 'px-3 py-1.5 text-sm',
+    md: 'px-4 py-2 text-base',
+    lg: 'px-5 py-2.5 text-lg',
+    xl: 'px-6 py-3 text-xl',
+  };
 
   // Define variant styles
   const variantStyles = {
@@ -39,8 +50,8 @@ const SelectComponent: React.FC<SelectComponentProps> = ({
       'bg-red-100 border-red-600 text-red-600 hover:bg-red-200 focus:ring-red-50',
   };
 
-  // Combine base styles, variant styles, and any custom className passed
-  const selectClassNames = `${baseStyles} ${variantStyles[variant]} ${className}`;
+  // Combine base styles, variant styles, size styles, and any custom className passed
+  const selectClassNames = `${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]} ${className}`;
 
   return (
     <div className="w-full max-w-sm mx-auto">
