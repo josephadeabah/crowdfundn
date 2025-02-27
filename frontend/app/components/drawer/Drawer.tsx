1b7677aaa6765b701f4dadec80cec750f585a3af
josephadeabah <abeansah.lx@gmail.com>
2025-02-27 18:51:41 +0000

    chore: clean code

--- frontend/app/components/drawer/Drawer.tsx
+++ frontend/app/components/drawer/Drawer.tsx
@@ -139,7 +139,7 @@ const AnimatedDrawer: React.FC<AnimatedDrawerProps> = ({
         </button>
 
         {/* Content */}
-        <div className="h-full overflow-y-auto p-2 md:p-6 pt-16">
+        <div className="h-full overflow-y-auto p-2 md:p-6 pt-16 [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-1 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2">
           {children}
         </div>
       </div>
