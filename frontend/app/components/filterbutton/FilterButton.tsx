0e965df4c7a22b7f260c21e49a0aad7889f0d9c0
josephadeabah <abeansah.lx@gmail.com>
2025-02-28 10:51:47 +0000

    fix: make filter button globally accessible

--- frontend/app/components/filterbutton/FilterButton.tsx
+++ frontend/app/components/filterbutton/FilterButton.tsx
@@ -1,7 +1,7 @@
 // components/FilterButton.tsx
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { Button } from '../button/Button';
+import { motion } from 'framer-motion';
 
 // components/FilterButton.tsx
 type FilterButtonProps = {
@@ -11,15 +11,17 @@ type FilterButtonProps = {
 
 const FilterButton: React.FC<FilterButtonProps> = ({ onClick, isActive }) => {
   return (
-    <Button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
       className={`flex items-center gap-2 ${
-        isActive ? 'bg-gray-200' : 'bg-gray-100'
-      } py-1 px-4 bg-white dark:bg-gray-900 dark:text-gray-50 rounded-full focus-visible:outline-none focus:ring-0 hover:outline-none hover:bg-gray-100 hover:text-gray-700 hover:scale-105 transition-transform duration-300`}
+        isActive ? 'bg-gray-50' : 'bg-white'
+      } py-1 px-4 dark:bg-gray-900 dark:text-gray-50 rounded-full focus-visible:outline-none focus:ring-0 hover:outline-none hover:bg-gray-100 hover:text-gray-700 hover:scale-105 transition-transform duration-300`}
       onClick={onClick}
     >
-      <FaSearch />
+      <FaSearch className="text-gray-500"/>
       Search
-    </Button>
+    </motion.button>
   );
 };
 
