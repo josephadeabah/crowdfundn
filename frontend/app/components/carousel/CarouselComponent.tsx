eeb728da7b3d84c87b064f5b18212d568e82ce53
josephadeabah <abeansah.lx@gmail.com>
2025-03-02 22:50:16 +0000

    chore: increase page size

--- frontend/app/components/carousel/CarouselComponent.tsx
+++ frontend/app/components/carousel/CarouselComponent.tsx
@@ -53,7 +53,7 @@ export interface CarouselProps {
 const CarouselComponent = ({
   children,
   title,
-  slidesToShow = 5,
+  slidesToShow = 2,
   autoplay = false,
   autoplaySpeed = 5000,
 }: CarouselProps) => {
@@ -65,7 +65,6 @@ const CarouselComponent = ({
     dots: false, // Hide dots
     infinite: false, // Disable infinite scrolling
     speed: 100,
-    slidesToShow: 3, // Show 3 cards per row
     slidesToScroll: 3, // Scroll 3 cards at a time
     rows: 2,
     slidesPerRow: 1,
