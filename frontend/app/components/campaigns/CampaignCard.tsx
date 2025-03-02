cd1635704c9113b7a941afe0c815386a6da7f825
josephadeabah <abeansah.lx@gmail.com>
2025-03-02 22:01:30 +0000

    feat: add carousel component to display campaigns

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -163,7 +163,7 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
                 initial="hidden"
                 animate="visible"
                 transition={{ duration: 0.5, delay: index * 0.1 }}
-                className="group relative dark:bg-gray-900 px-3 h-full dark:text-gray-50 cursor-pointer overflow-hidden rounded-lg"
+                className="group relative dark:bg-gray-900 px-3 py-3 h-full dark:text-gray-50 cursor-pointer overflow-hidden rounded-lg"
               >
                 <Link
                   href={`/campaign/${campaign.id}?${generateRandomString()}`}
