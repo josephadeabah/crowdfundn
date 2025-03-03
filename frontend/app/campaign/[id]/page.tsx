d867f471abfa43f6e8c063f8effb9fb7e07291ba
josephadeabah <abeansah.lx@gmail.com>
2025-03-03 19:12:23 +0000

    chore: make sticky tabs for one campaign page view

--- frontend/app/campaign/[id]/page.tsx
+++ frontend/app/campaign/[id]/page.tsx
@@ -192,7 +192,7 @@ const SingleCampaignPage: React.FC = () => {
                 />
               </div>
             </div>
-            <div className={`relative ${isSticky ? 'sticky top-0 bg-white z-50 shadow-md' : ''}`}>
+            <div className={`relative ${isSticky ? 'sticky top-0 bg-white z-50 shadow-md px-2' : ''}`}>
               <div className="flex items-center mb-6">
                 <button
                   onClick={() => scrollTabs('left')}
