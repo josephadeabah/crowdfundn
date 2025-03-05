2c0e53ceadf72ad1071d34b6846e17b1ffe000b5
josephadeabah <abeansah.lx@gmail.com>
2025-03-05 08:04:34 +0000

    fix: place items left in carousel

--- frontend/app/components/campaigns/RewardCard.tsx
+++ frontend/app/components/campaigns/RewardCard.tsx
@@ -54,12 +54,12 @@ const RewardCard: React.FC<RewardCardsProps> = ({
               initial="hidden"
               animate="visible"
               transition={{ duration: 0.5 }}
-              className="group relative dark:bg-gray-900 flex justify-items-start items-center px-3 py-3 h-full dark:text-gray-50 cursor-pointer overflow-hidden rounded-lg"
+              className="group relative dark:bg-gray-900 px-3 py-3 h-full dark:text-gray-50 cursor-pointer overflow-hidden rounded-lg"
             >
               <Link
                 href={`/campaign/${campaign.id}?tab=donate&${generateRandomString()}`}
               >
-                <div className="grid grid-cols-1 justify-start items-start h-full">
+                <div className="grid grid-cols-1 h-full">
                   {/* Reward Image */}
                   <div className="relative w-full h-32">
                     <Image
