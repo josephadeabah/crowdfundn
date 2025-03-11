0299bbe7be0dc8d7dcb29db0551815f2a3f1eb20
josephadeabah <abeansah.lx@gmail.com>
2025-03-11 15:55:14 +0000

    feat: add video preview

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -200,7 +200,7 @@ const Hero = () => {
           <div className="lg:-mr-5 xl:-mr-14  reveal reveal-delay-4">
           <VideoPlayer
             src="https://www.youtube.com/watch?v=ImV9722mQdc"
-            posterImage="https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2340&auto=format&fit=crop"
+            posterImage="/undraw_gifts_0twc.svg"
             title="Introducing Gift for Fundraisers"
             className="aspect-video md:aspect-[16/9] rounded-2xl overflow-hidden shadow-2xl"
           />
