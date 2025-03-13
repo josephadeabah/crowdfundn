7a8db1b3edecf44558ebe3ad057bd20c90b1c06b
josephadeabah <abeansah.lx@gmail.com>
2025-03-13 23:24:50 +0000

    fix: dependencies conflict

--- frontend/app/components/campaign/CampaignHeader.tsx
+++ frontend/app/components/campaign/CampaignHeader.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const CampaignHeader = () => {
   return (
-    <header className="w-full mb-3 text-left">
+    <div className="w-full mb-3 text-left">
       <p className="text-emerald-600">
         Create your crowdfunding campaign. You can choose a templates to start
         with.
       </p>
-    </header>
+    </div>
   );
 };
 
