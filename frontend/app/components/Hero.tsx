c8247157a8feebaeff236a926afac230047f909b
josephadeabah <abeansah.lx@gmail.com>
2025-03-15 02:00:41 +0000

    chore: change welcome video

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -260,7 +260,7 @@ const Hero = () => {
       {isVideoOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
           <VideoPlayer
-            videoSrc="/welcome.mp4"
+            videoSrc="/how-tos-with-bantuhive.webm"
             isOpen={isVideoOpen}
             onClose={closeVideo}
           />
