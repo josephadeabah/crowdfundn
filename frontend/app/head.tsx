665c11ae5abbe0a9b530aa552b7537cdf1db3d3e
josephadeabah <abeansah.lx@gmail.com>
2025-03-17 11:43:19 +0000

    feat: add google tag

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -87,6 +87,18 @@ export default function Head() {
           }),
         }}
       />
+      {/* Google Tag */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YWLECWF7W7"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YWLECWF7W7');
+          `,
+        }}
+      />
     </head>
   );
 }
