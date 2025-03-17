a31c8e0d5159d528b7a15918c77e9326c6d81620
josephadeabah <abeansah.lx@gmail.com>
2025-03-17 20:24:09 +0000

    fix: at time schedule for transfer notif

--- frontend/app/account/Transfers.tsx
+++ frontend/app/account/Transfers.tsx
@@ -124,7 +124,7 @@ export default function Transfers() {
         Review your transfer history or request new transfers.
         <span className="text-cyan-500">
           {' '}
-          You can request for transfer weekdays at 10:30 am
+          You can request for transfer weekdays from 10:30 am
         </span>
       </p>
 
