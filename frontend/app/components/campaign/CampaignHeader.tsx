e111735c3aad8e9a294c86ec304077e039864477
josephadeabah <abeansah.lx@gmail.com>
2025-03-22 23:34:53 +0000

    fix: show loading for campaigns

--- frontend/app/components/campaign/CampaignHeader.tsx
+++ frontend/app/components/campaign/CampaignHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CampaignHeader = () => {
   return (
     <div className="w-full mb-3 text-left">
-      <p className="text-emerald-600">
+      <p className="text-gray-200">
         Create your crowdfunding campaign. You can choose a templates to start
         with.
       </p>
