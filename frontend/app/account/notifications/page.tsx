5615eaf2ebbfa08f8859152a9afac1c8776b2a55
josephadeabah <abeansah.lx@gmail.com>
2025-03-28 07:51:28 +0000

    fix: layout on notification page

--- frontend/app/account/notifications/page.tsx
+++ frontend/app/account/notifications/page.tsx
@@ -18,8 +18,8 @@ export default function NotificationsPage() {
     <div className="w-full bg-white dark:bg-gray-800 min-h-screen">
       <div className="max-w-6xl w-full px-4 sm:px-6 lg:px-12 py-8 ml-auto">
         <div className="flex flex-col lg:flex-row gap-8">
-          {/* Left sidebar - can be used for filters or other content */}
-          <div className="lg:w-1/4">
+          {/* Left sidebar - 15% width */}
+          <div className="lg:w-[15%]"> {/* Changed from lg:w-1/4 to lg:w-[15%] */}
             <div className="sticky top-8 space-y-4">
               <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
                 Notifications
@@ -28,13 +28,14 @@ export default function NotificationsPage() {
                 <button className="w-full text-left px-4 py-2 rounded-lg bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white">
                   All Notifications
                 </button>
+                {/* Add more sidebar content as needed */}
               </div>
             </div>
           </div>
 
-          {/* Main notifications content - positioned on the right */}
-          <div className="lg:w-3/4">
-            <NotificationsComponent notification={notifications} />
+          {/* Main notifications content - 85% width */}
+          <div className="lg:w-[85%]"> {/* Changed from lg:w-3/4 to lg:w-[85%] */}
+            <NotificationsComponent notification={notifications} /> {/* Fixed prop name */}
           </div>
         </div>
       </div>
