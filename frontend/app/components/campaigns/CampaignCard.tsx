7ff2d7f8c6cb9874007b93d51c97732b8b9a711b
josephadeabah <abeansah.lx@gmail.com>
2025-04-01 23:59:10 +0000

    fix: img broken

--- frontend/app/components/campaigns/CampaignCard.tsx
+++ frontend/app/components/campaigns/CampaignCard.tsx
@@ -162,7 +162,7 @@ const CampaignCard: React.FC<CampaignCardProps> = ({
         type={toast.type}
       />
       <div
-        className="group relative overflow-hidden rounded-lg bg-background hover:shadow-md transition-all duration-300 animate-fade-up h-full flex flex-col text-xs"
+        className="group relative overflow-hidden rounded-lg bg-background hover:bg-gray-50 hover:shadow-md transition-all duration-300 animate-fade-up h-full flex flex-col text-xs"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
