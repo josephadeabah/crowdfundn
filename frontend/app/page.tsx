f6f809843b4017e0caa30fc3357134d0b873cb87
josephadeabah <abeansah.lx@gmail.com>
2025-04-07 00:56:12 +0000

    fix: minimal style applied to blog list

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -50,7 +50,7 @@ const HomePage = () => {
         </div>
         <div className="w-full bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <BlogPosts />
+            <BlogPosts />
           </div>
         </div>
         <div className="w-full">
