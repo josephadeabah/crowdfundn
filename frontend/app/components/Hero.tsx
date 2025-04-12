80fc97a86d3e0a81645f01afbc7264ffc811f961
josephadeabah <abeansah.lx@gmail.com>
2025-04-12 13:02:08 +0000

    fix: flex action button on hero on mobile

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -139,20 +139,20 @@ const Hero = () => {
                 </p>
 
                 <div className="flex animate-fade-up animate-delay-300">
-  <div className="flex flex-col xs:flex-row justify-between w-full gap-4">
-    <button className="group px-4 py-3 bg-green-600 text-primary-foreground rounded-md hover:bg-green-400 transition-colors flex items-center justify-center gap-2 whitespace-nowrap flex-shrink-0">
-      <Link
-        href={`${user ? '/account/dashboard/create' : '/auth/register'}`}
-      >
-        Fundraise Now for Free
-      </Link>
-      <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
-    </button>
-    <button className="px-4 py-3 bg-transparent border border-border text-foreground rounded-md hover:bg-muted transition-colors whitespace-nowrap flex-shrink-0">
-      <Link href="/how-it-works">How It Works</Link>
-    </button>
-  </div>
-</div>
+                  <div className="flex flex-col xs:flex-row justify-between w-full gap-4">
+                    <button className="group px-4 py-3 bg-green-600 text-primary-foreground rounded-md hover:bg-green-400 transition-colors flex items-center justify-center gap-2 whitespace-nowrap flex-shrink-0">
+                      <Link
+                        href={`${user ? '/account/dashboard/create' : '/auth/register'}`}
+                      >
+                        Fundraise Now for Free
+                      </Link>
+                      <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
+                    </button>
+                    <button className="px-4 py-3 bg-transparent border border-border text-foreground rounded-md hover:bg-muted transition-colors whitespace-nowrap flex-shrink-0">
+                      <Link href="/how-it-works">How It Works</Link>
+                    </button>
+                  </div>
+                </div>
 
                 <div className="mt-12 flex flex-col sm:flex-row items-center gap-4 animate-fade-up animate-delay-400">
                   <div className="flex -space-x-3">
