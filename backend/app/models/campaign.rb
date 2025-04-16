f1d9ea9a3416dab13a77032cbeffc9ed8b9b4733
josephadeabah <abeansah.lx@gmail.com>
2025-04-16 16:05:20 +0000

    fix: create a single base controller for campaigns types

--- backend/app/models/campaign.rb
+++ backend/app/models/campaign.rb
@@ -108,6 +108,9 @@ class Campaign < ApplicationRecord
         promotion_frequency: promotion_frequency,
         promotion_duration: promotion_duration
       },
+      rewards: rewards,
+      updates: updates,
+      comments: comments,
       fundraiser: {
         id: fundraiser.id,
         name: fundraiser.full_name,
@@ -115,6 +118,8 @@ class Campaign < ApplicationRecord
         currency_symbol: fundraiser.currency_symbol,
         profile: fundraiser.profile
       },
+      total_days: total_days,
+      remaining_days: remaining_days,
       favorited: options[:user] ? options[:user].favorited_campaigns.include?(self) : false
     )
     end
