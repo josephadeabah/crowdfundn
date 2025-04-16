b47b8a642c31db58eb55ae6973808ddda71b478b
josephadeabah <abeansah.lx@gmail.com>
2025-04-16 22:20:43 +0000

    fix: rename equity campaign id to campaign id in campaign team members

--- backend/db/migrate/20250416220926_rename_equity_campaign_id_to_campaign_id_in_campaign_team_members.rb
+++
@@ -1,5 +0,0 @@
-class RenameEquityCampaignIdToCampaignIdInCampaignTeamMembers < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :campaign_team_members, :equity_campaign_id, :campaign_id
-  end
-end
