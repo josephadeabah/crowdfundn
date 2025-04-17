4af7a8603b23da098c6c852a0d721ec9e09893aa
josephadeabah <abeansah.lx@gmail.com>
2025-04-17 23:32:31 +0000

    fix: extend equity campaign creation data and UI

--- frontend/app/components/campaign/CampaignDetails.tsx
+++ frontend/app/components/campaign/CampaignDetails.tsx
@@ -194,7 +194,7 @@ const CampaignDetails = ({
           </div>
 
           {!showEquitySections && (
-            <div className="w-full mb-2 border border-gray-200 rounded-md">
+            <div className="w-full mb-2 border border-gray-200 rounded-md mt-6">
               <span className="text-sm font-semibold mb-4 text-gray-500 p-4 italic">
                 Only use the below if you're raising to give out equity to
                 investors otherwise continue to content
