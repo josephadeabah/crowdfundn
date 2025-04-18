a97538881e285b8bc6a96c8058d1eddf47481c47
josephadeabah <abeansah.lx@gmail.com>
2025-04-18 07:40:08 +0000

    fix: move uplaod function to contract terms

--- frontend/app/components/equity/TermsContract.tsx
+++ frontend/app/components/equity/TermsContract.tsx
@@ -19,7 +19,7 @@ const TOOLTIP_CONTENT = `
 📄 Contracts Documents for Investors:
 • Form C or equivalent
 • Any Required Agreement document neccessary for the investment
-• Investors will have to keep a copy of these documents for their records
+• We'll show these documents to Investors to have and keep a copy of these documents for their records
 `;
 
 interface TermsContractProps {
