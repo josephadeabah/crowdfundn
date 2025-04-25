d96841978503d208edebbc77858abeb88bdb126f
josephadeabah <abeansah.lx@gmail.com>
2025-04-25 21:57:44 +0000

    fix: backers data

--- frontend/app/campaign/CampaignBackers.tsx
+++ frontend/app/campaign/CampaignBackers.tsx
@@ -21,7 +21,7 @@ const CampaignBackers: React.FC<CampaignBackersProps> = ({
         {isEquityCampaign ? 'Investors' : 'Backers'}
       </h3>
       <DonationList
-        donations={[]}
+        donations={campaign?.backers || []}
         fundraiserCurrency={fundraiserCurrency}
         campaignId={String(campaign?.id)}
       />
