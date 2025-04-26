689e55f777bd6401eb4c0cdfdb0f43899689f4e5
josephadeabah <abeansah.lx@gmail.com>
2025-04-26 17:55:56 +0000

    feat: add investment calculator guide

--- frontend/app/investment-calculator/page.tsx
+++ frontend/app/investment-calculator/page.tsx
@@ -5,9 +5,6 @@ import InvestmentCalculatorColorfulTable from '@/app/components/equitycalculator
 export default function CrowdfundingPage() {
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-bold mb-6">
-        Calculate your potential returns on investment.
-      </h1>
       <InvestmentCalculatorColorfulTable
         futureValuations={[10, 20, 30, 50, 100]}
       />
