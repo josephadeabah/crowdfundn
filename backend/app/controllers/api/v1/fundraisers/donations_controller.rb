257ee89fc8ea3a9f73d0f22d26f06ece9e488eb3
josephadeabah <abeansah.lx@gmail.com>
2025-06-05 12:39:11 +0000

    fix: slug in url redirect for donation

--- backend/app/controllers/api/v1/fundraisers/donations_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/donations_controller.rb
@@ -97,7 +97,12 @@ module Api
         
           # Generate a secure random UUID and append it to the redirect_url as a query parameter
           secure_random_uuid = SecureRandom.uuid
-          redirect_url = Rails.application.routes.url_helpers.campaign_url(campaign.id, host: 'bantuhive.com') + "?#{secure_random_uuid}"
+            # Use campaign.slug if available, otherwise fall back to id
+          campaign_identifier = campaign.slug || campaign.id
+          redirect_url = Rails.application.routes.url_helpers.campaign_url(
+            campaign_identifier, 
+            host: 'bantuhive.com'
+          ) + "?#{secure_random_uuid}"
           donation.email = params[:donation][:email]
           donation.amount = params[:donation][:amount]
           donation.phone = params[:donation][:phone]
