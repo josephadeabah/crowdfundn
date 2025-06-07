b790de7ca4b044e95a548639640db9793bc252ae
josephadeabah <abeansah.lx@gmail.com>
2025-06-07 00:19:52 +0000

    fix: app title

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Fundraising Made Easy for Africa & Diaspora</title>
+      <title>Bantuhive | The Best All-in-One Fundraiser Management Software Platform</title>
       <meta
         name="description"
         content="The All-in-One Fundraiser Management Software Platform"
