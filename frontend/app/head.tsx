177c7d923d19f48b85e82605f7db3295667eef3a
josephadeabah <abeansah.lx@gmail.com>
2025-06-12 23:56:55 +0000

    chore: change text title

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>
-        BantuHIVE | The Best Fundraiser Management Software Platform
+        All-in-One Fundraiser Management Software Platform
       </title>
       <meta
         name="description"
