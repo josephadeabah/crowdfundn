70b53e63b113ae7f5fa0f48a149eed589a396f2b
josephadeabah <abeansah.lx@gmail.com>
2025-07-20 18:00:32 +0000

    feat: add dynamic bg

--- frontend/app/components/galaxybg/DynamicBackground.tsx
+++ frontend/app/components/galaxybg/DynamicBackground.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useRef, useMemo, useCallback } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Float, Preload } from '@react-three/drei';
