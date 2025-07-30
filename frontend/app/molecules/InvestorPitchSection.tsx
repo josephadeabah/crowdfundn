e3d600fe0fdfc4c6b0534bae021c33fd668d60c9
josephadeabah <abeansah.lx@gmail.com>
2025-07-30 03:50:23 +0000

    fix: pitchhome

--- frontend/app/molecules/InvestorPitchSection.tsx
+++ frontend/app/molecules/InvestorPitchSection.tsx
@@ -39,7 +39,7 @@ const InvestorPitchSection: React.FC = () => {
           className="w-1/3 object-cover h-full"
         />
         <img
-          src="/busshot3.avif"
+          src="/busshot2.avif"
           alt="background2"
           className="w-1/3 object-cover h-full"
         />
