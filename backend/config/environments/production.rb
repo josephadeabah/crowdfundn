c7e1eade1b5c30b6721971f593cb9df0c8426aa8
josephadeabah <abeansah.lx@gmail.com>
2025-08-03 16:06:40 +0000

    fix: host config

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -97,8 +97,6 @@ Rails.application.configure do
   
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "bantuhive-app-hldzm.ondigitalocean.app"
-  config.hosts << "api.bantuhive.com"
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
