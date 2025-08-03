8fe9dc67b90c299ea485a8c1f320d936ea11e1ff
josephadeabah <abeansah.lx@gmail.com>
2025-08-03 11:06:55 +0000

    fix: carousel home

--- frontend/app/components/campaigns/FeaturedCampaigns.tsx
+++ frontend/app/components/campaigns/FeaturedCampaigns.tsx
@@ -76,7 +76,7 @@ const FeaturedCampaigns = () => {
           </Button>
         </Link>
       </div>
-      <div className="bg-gray-50 py-12">
+      <div className="">
         <FundingTypes />
       </div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
