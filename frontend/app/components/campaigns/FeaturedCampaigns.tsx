95b76643866a57e712bfb9e753f7209cbc8c5f07
josephadeabah <abeansah.lx@gmail.com>
2025-08-03 10:59:04 +0000

    fix: carousel home

--- frontend/app/components/campaigns/FeaturedCampaigns.tsx
+++ frontend/app/components/campaigns/FeaturedCampaigns.tsx
@@ -54,7 +54,7 @@ const FeaturedCampaigns = () => {
           error={error}
         />
       </div>
-      <div className='bg-gray-50 py-12'>
+      <div className="bg-gray-50 py-12">
         <InvestorPitchSection />
       </div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
