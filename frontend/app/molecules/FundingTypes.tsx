79f74fe08f6fa3d46464b74ef09003ab9eba38e6
josephadeabah <abeansah.lx@gmail.com>
2025-08-03 09:42:37 +0000

    fix: carousel home

--- frontend/app/molecules/FundingTypes.tsx
+++ frontend/app/molecules/FundingTypes.tsx
@@ -3,8 +3,8 @@ import MarketingMediaCarousel from './MarketingMediaCarousel';
 
 const FundingTypes = () => {
   return (
-    <div className="px-2 sm:px-3">
-      <div className="text-center mb-12">
+    <div className="sm:px-2">
+      <div className="text-center mb-12 px-2">
         <h2 className="text-3xl font-bold mb-4">
           From tomorrow’s unicorns to local coffee shops
         </h2>
