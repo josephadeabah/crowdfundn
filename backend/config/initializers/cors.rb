52b01de3f8c852f24655727825f35f13207ce1eb
josephadeabah <abeansah.lx@gmail.com>
2025-08-04 03:10:43 +0000

    fix: cors issue

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,11 +1,11 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://www.bantuhive.com'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://www.bantuhive.com'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true,
-      max_age: 600
-  end
-end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true,
+#       max_age: 600
+#   end
+# end
