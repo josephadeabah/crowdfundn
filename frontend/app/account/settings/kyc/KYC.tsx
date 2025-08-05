672bc9a93cfc7b4b8e2671885429d192fdd69ad4
josephadeabah <abeansah.lx@gmail.com>
2025-08-05 12:38:56 +0000

    fix: make steps vertical

--- frontend/app/account/settings/kyc/KYC.tsx
+++ frontend/app/account/settings/kyc/KYC.tsx
@@ -21,7 +21,7 @@ const KYC = () => {
   return (
     <div className="min-h-screen">
       <div className="container mx-auto py-10">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           <div className="text-center mb-8">
             <h1 className="text-3xl font-bold mb-2">
               {userType === 'creator'
