ca944b1427a4ae0ee5b9f82caba9419d581b5c96
josephadeabah <abeansah.lx@gmail.com>
2025-08-05 01:38:19 +0000

    fix: break kyc component into reusable modules

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -163,11 +163,11 @@ const ProfileTabs = () => {
                   <a
                     key={label}
                     href={`#${label}`}
-                    className={`py-3 px-3 h-full whitespace-nowrap text-sm font-medium md:text-base transform transition-transform duration-300 ${
+                    className={`flex items-center gap-3 px-4 py-3 text-sm font-medium rounded-md transition-all duration-200 whitespace-nowrap min-w-fit lg:min-w-full lg:w-full relative group ${
                       isActive
                         ? 'border-b-2 border-2 border-dashed md:border-b-0 md:border-l-2 md:border-r-0 border-orange-200 text-orange-400 dark:text-orange-600'
                         : 'border-transparent text-gray-600 hover:bg-gray-100 dark:hover:bg-neutral-700 hover:text-gray-900 dark:text-neutral-400 dark:hover:text-gray-950'
-                    } flex items-center focus:outline-none ${
+                    }  focus:outline-none ${
                       isOnboarding
                         ? 'bg-green-600 text-white dark:bg-orange-700'
                         : ''
