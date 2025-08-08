f9c16579d803881d36daf9679c90c3ca94b1ac7b
josephadeabah <abeansah.lx@gmail.com>
2025-08-08 04:28:51 +0000

    fix: display statuses on campaign cards with diff

--- frontend/app/components/ui/calender.tsx
+++ frontend/app/components/ui/calender.tsx
@@ -62,9 +62,9 @@ function Calendar({
         IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      captionLayout="dropdown-buttons"
-      fromYear={1924}
-      toYear={new Date().getFullYear() - 18}
+      // captionLayout="dropdown-buttons"
+      // fromYear={1924}
+      // toYear={new Date().getFullYear() - 18}
       {...props}
     />
   );
