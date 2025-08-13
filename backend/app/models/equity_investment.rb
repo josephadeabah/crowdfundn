df7e25773a9db4a72a92cfc50d58287fff1be00c
josephadeabah <abeansah.lx@gmail.com>
2025-08-13 23:34:52 +0000

    chore: restore equity inv and remove sti on donation and equity

--- backend/app/models/equity_investment.rb
+++ backend/app/models/equity_investment.rb
@@ -10,7 +10,7 @@ class EquityInvestment < ApplicationRecord
   validates :certificate_number, uniqueness: true, allow_nil: true
   validates :transaction_reference, uniqueness: true, allow_nil: true
   validates :email, presence: true
-  validates :phone, allow_blank: true
+  validates :phone, presence: false # This allows phone to be blank/nil
 
   enum :status, {
     pending: 0,
