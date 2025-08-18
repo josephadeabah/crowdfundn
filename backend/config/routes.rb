1886205dd719ec8610021d7989e1bcb59d182d95
josephadeabah <abeansah.lx@gmail.com>
2025-08-18 02:30:47 +0000

    feat: add public investors

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -116,7 +116,6 @@ Rails.application.routes.draw do
 
         # Fundraiser-facing equity operations
         resources :equity_campaigns, only: [] do
-              get 'public_investments', to: 'equity_investments#public_investments'
           resources :equity_investments, only: [:index, :create, :update, :destroy] do
             collection do
               get :public_investments
@@ -127,6 +126,7 @@ Rails.application.routes.draw do
             member do
               get :certificate_status
               post :generate_certificate
+              get 'public_investments', to: 'equity_investments#public_investments'
             end
           end
         end
