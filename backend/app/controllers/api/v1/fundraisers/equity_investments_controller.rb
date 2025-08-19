e9849b3655fa89b0fc5d0641a23dc925a806f246
josephadeabah <abeansah.lx@gmail.com>
2025-08-19 08:20:58 +0000

    fix: 404 not found for create

--- backend/app/controllers/api/v1/fundraisers/equity_investments_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/equity_investments_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     module Fundraisers
       class EquityInvestmentsController < ApplicationController
-        before_action :authenticate_request
-        before_action :set_campaign, only: [:create, :public_investments]
+        before_action :authenticate_request, except: [:public_investments]
+        before_action :set_campaign, only: [:create]
         before_action :set_investment, only: [:show, :update, :destroy, :certificate_status]
 
         def public_investments
