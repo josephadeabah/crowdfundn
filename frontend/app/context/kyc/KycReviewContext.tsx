812ff28d8cfb880d673715a1e7eaa675b1b00791
josephadeabah <abeansah.lx@gmail.com>
2025-08-22 02:41:27 +0000

    fix: remove pundit authorization

--- frontend/app/context/kyc/KycReviewContext.tsx
+++ frontend/app/context/kyc/KycReviewContext.tsx
@@ -128,7 +128,7 @@ export const KycReviewProvider = ({ children }: { children: ReactNode }) => {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/kyc/stats`,
+        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/kyc/kycs/stats`,
         {
           method: 'GET',
           headers: {
