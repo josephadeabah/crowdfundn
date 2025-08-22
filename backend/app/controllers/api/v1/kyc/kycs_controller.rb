08693843b8e458d2dc892d42c85744d1e0688dc9
josephadeabah <abeansah.lx@gmail.com>
2025-08-22 14:13:39 +0000

    fix: add missing fields on UI

--- backend/app/controllers/api/v1/kyc/kycs_controller.rb
+++ backend/app/controllers/api/v1/kyc/kycs_controller.rb
@@ -3,7 +3,7 @@ module Api
   module V1
     module Kyc
       class KycsController < ApplicationController
-        before_action :authenticate_request
+        before_action :authenticate_request, except: [:show]
         before_action :set_kyc, only: [:show, :update, :destroy, :submit, :documents, :verify, :reject, :request_info, :upload_document]
         before_action :authorize_user_access, only: [:update, :destroy, :submit, :documents]
         before_action :authorize_admin, only: [:all_needs_review, :stats, :verify, :reject, :request_info]
