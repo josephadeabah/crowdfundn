104df9817422fdee127225ef7f555b738b601b7a
josephadeabah <abeansah.lx@gmail.com>
2025-08-29 22:49:10 +0000

    fix: saving data to pledges

--- backend/app/controllers/api/v1/fundraisers/equity_investments_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/equity_investments_controller.rb
@@ -276,9 +276,9 @@ module Api
           }
 
           # Add signature data if available
-          if @current_user.latest_kyc&.signature_data.present?
-            metadata[:investor_signature_data] = @current_user.latest_kyc.signature_data
-          end
+          # if @current_user.latest_kyc&.signature_data.present?
+          #   metadata[:investor_signature_data] = @current_user.latest_kyc.signature_data
+          # end
 
           if investment.reward_id
             metadata[:reward] = {
