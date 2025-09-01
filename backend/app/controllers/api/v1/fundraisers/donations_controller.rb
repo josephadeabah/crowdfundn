af89eecbd82426b2083618cb51303dd6f3343735
josephadeabah <abeansah.lx@gmail.com>
2025-09-01 23:54:06 +0000

    fix: comment for campaign and equity campaign

--- backend/app/controllers/api/v1/fundraisers/donations_controller.rb
+++ backend/app/controllers/api/v1/fundraisers/donations_controller.rb
@@ -183,7 +183,7 @@ module Api
           paystack_service = PaystackService.new
           response = paystack_service.initialize_transaction(
             email: donation.email,
-            amount: donation.amount
+            amount: donation.amount,
             plan: donation.plan,
             callback_url: redirect_url,
             metadata: metadata,
