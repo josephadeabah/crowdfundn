8e89fa886af153a110f95edb47eca680af3e865a
josephadeabah <abeansah.lx@gmail.com>
2025-09-07 18:46:52 +0000

    fix: change growth pricing to 299

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -234,7 +234,7 @@ const ProfileTabs = () => {
               ) : (
                 // Show upgrade button if user doesn't have premium
                 <Link
-                  href="/info/upgrade"
+                  href="/account#Settings"
                   className="w-full py-2 px-4 bg-gradient-to-r from-green-500 to-green-600 text-white rounded-full flex items-center justify-center hover:from-green-600 hover:to-green-700 transition-colors duration-300 shadow-sm"
                 >
                   <FaCashRegister className="mr-2" />
