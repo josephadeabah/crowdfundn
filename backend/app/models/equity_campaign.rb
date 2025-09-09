4ccbcb1f17ed7c4e07929a94400bc26058865e26
josephadeabah <abeansah.lx@gmail.com>
2025-09-09 21:23:24 +0000

    fix: reccuring subs issue

--- backend/app/models/equity_campaign.rb
+++ backend/app/models/equity_campaign.rb
@@ -263,7 +263,7 @@ class EquityCampaign < Campaign
   end
   
   def founder_equity_within_bounds
-    if founder_equity_percentage > 85
+    if founder_equity_percentage > 90
       errors.add(:base, "Founder equity allocation is too high for a credible offering")
     end
     
