f4016ce57dfb4b171fb65ae6ea3bda6e0d88ef3f
josephadeabah <abeansah.lx@gmail.com>
2025-09-12 10:16:40 +0000

    fix: remove dark mode classes

--- frontend/app/loaders/ProfileTabsLoader.tsx
+++ frontend/app/loaders/ProfileTabsLoader.tsx
@@ -6,7 +6,7 @@ const ProfileTabsLoader = () => {
   return (
     <div className="max-w-7xl mx-auto flex flex-col md:flex-row h-screen">
       {/* Tabs Menu Skeleton */}
-      <div className="w-full md:w-[13%] border-b h-auto md:h-screen md:border-b-0 md:border-r-2 border-dashed border-red-200 dark:border-neutral-700">
+      <div className="w-full md:w-[13%] border-b h-auto md:h-screen md:border-b-0 md:border-r-2 border-dashed border-red-200">
         <nav className="flex md:flex-col w-full space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible">
           {Array(6)
             .fill(null)
@@ -20,7 +20,7 @@ const ProfileTabsLoader = () => {
       </div>
 
       {/* Tab Content Skeleton */}
-      <div className="w-full bg-white dark:bg-gray-900 px-4 overflow-auto flex-1 h-full md:h-screen">
+      <div className="w-full bg-white px-4 overflow-auto flex-1 h-full md:h-screen">
         <div className="h-full space-y-6 p-4">
           {/* Simulating grid sections */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
