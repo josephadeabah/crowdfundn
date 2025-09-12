b84d05c13629543f967558d9c58c2257b22bb29f
josephadeabah <abeansah.lx@gmail.com>
2025-09-12 19:58:19 +0000

    chore: test paystack subscription

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -232,7 +232,7 @@ const ProfileTabs = () => {
             <div className="sticky bottom-0 bg-white pt-2 pb-4 px-3 border-t border-dashed border-orange-200">
               {hasPremium ? (
                 <div className="w-full py-2 px-4 border-2 border-purple-600 text-purple-800 rounded-full text-center shadow-sm">
-                  <div className="flex text-sm items-center justify-center mb-1">
+                  <div className="flex text-xs items-center justify-center mb-1">
                     <FaCrown className="mr-2" />
                     <div className="opacity-90">
                       {subscription.current_plan?.name} Plan
