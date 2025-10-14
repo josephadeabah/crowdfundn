01e2108a6a95dde61dfd36036f33bdf17543c336
josephadeabah <abeansah.lx@gmail.com>
2025-10-14 22:43:10 +0000

    fix: add risk warning to footer

--- frontend/app/molecules/FooterWarning.tsx
+++ frontend/app/molecules/FooterWarning.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const FooterWarning: React.FC = () => {
   return (
-    <div className="bg-white border-t border-gray-200">
-      <div className="max-w-7xl mx-auto px-4">
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto px-4 mb-6">
         <div className="">
           <h4 className="font-bold text-sm mb-4 text-gray-900">
             Important Investment Notice
