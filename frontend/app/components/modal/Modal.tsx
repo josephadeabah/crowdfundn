474717ece24031a26e7b225f396e18b405da5092
josephadeabah <abeansah.lx@gmail.com>
2025-10-17 20:16:57 +0000

    fix: few corrections

--- frontend/app/components/modal/Modal.tsx
+++ frontend/app/components/modal/Modal.tsx
@@ -74,7 +74,7 @@ const Modal: React.FC<ModalProps> = ({
         onInteractOutside={closeOnBackdropClick ? undefined : (e) => e.preventDefault()}
       >
         {/* Custom close button */}
-        <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+        <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
           <FaTimes className="h-6 w-6" />
           <span className="sr-only">Close</span>
         </DialogClose>
