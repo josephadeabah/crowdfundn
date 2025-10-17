79d8f882636ec331bb8c4b00e39a1d9c85882fa4
josephadeabah <abeansah.lx@gmail.com>
2025-10-17 19:44:47 +0000

    fix: few corrections

--- frontend/app/account/EquityInvestments.tsx
+++ frontend/app/account/EquityInvestments.tsx
@@ -425,7 +425,7 @@ const EquityInvestments = () => {
                 value={cancellationReason}
                 onChange={(e) => setCancellationReason(e.target.value)}
                 placeholder="Please provide a reason for cancelling this investment..."
-                className="mt-1 focus:outline-none focus:shadow-none focus:border-none focus:border-0"
+                className="mt-1"
               />
             </div>
             {selectedInvestment && (
