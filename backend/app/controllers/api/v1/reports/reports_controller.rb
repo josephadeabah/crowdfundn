20394c2819ab5929d6e9631c4f59e37612b413b3
josephadeabah <abeansah.lx@gmail.com>
2025-10-27 22:26:59 +0000

    feat: add campaign user reporting

--- backend/app/controllers/api/v1/reports/reports_controller.rb
+++ backend/app/controllers/api/v1/reports/reports_controller.rb
@@ -192,8 +192,8 @@ module Api
         end
 
         def authorize_admin
-          unless @current_user&.admin?
-            render json: { error: 'Unauthorized' }, status: :unauthorized
+          unless @current_user&.has_role?('Admin') || @current_user&.admin?
+            render json: { error: 'Unauthorized. Admin access required.' }, status: :unauthorized
           end
         end
       end
