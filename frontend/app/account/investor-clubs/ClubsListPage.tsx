e4c1e57cea9523af026255eb44192e59e9af9e23
josephadeabah <abeansah.lx@gmail.com>
2025-11-06 16:00:49 +0000

    feat: add club membership

--- frontend/app/account/investor-clubs/ClubsListPage.tsx
+++ frontend/app/account/investor-clubs/ClubsListPage.tsx
@@ -179,7 +179,7 @@ const ClubsListPage: React.FC = () => {
     if (club.club_type === 'private') {
       return {
         label: actionLoading === club.id ? 'Requesting...' : 'Request to Join',
-        style: 'bg-orange-200 text-orange-700 hover:bg-orange-600',
+        style: 'bg-orange-100 text-orange-800 hover:bg-orange-600 hover:text-white',
         onClick: (e: React.MouseEvent) => handleJoinRequest(club, e),
         disabled: actionLoading === club.id,
       };
