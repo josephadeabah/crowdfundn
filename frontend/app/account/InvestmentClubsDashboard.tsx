6814a10dc24f33f10f823844de0234c93f7d1c0e
josephadeabah <abeansah.lx@gmail.com>
2025-11-10 20:36:51 +0000

    fix: break investment club dashboard into sep components

--- frontend/app/account/InvestmentClubsDashboard.tsx
+++ frontend/app/account/InvestmentClubsDashboard.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import AlertPopup from '@/app/components/alertpopup/AlertPopup';
 import { useClubData } from './investor-clubs/hooks/useClubData';
 import { investmentService } from './investor-clubs/clubservice';
