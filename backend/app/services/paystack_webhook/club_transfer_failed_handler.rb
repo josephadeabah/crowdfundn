b76efdc85aa531e6d6d8e4d405e2770b9712f228
josephadeabah <abeansah.lx@gmail.com>
2025-11-16 21:11:31 +0000

    fix: updates transfers modal design

--- backend/app/services/paystack_webhook/club_transfer_failed_handler.rb
+++ backend/app/services/paystack_webhook/club_transfer_failed_handler.rb
@@ -44,12 +44,12 @@ class PaystackWebhook::ClubTransferFailedHandler
       end
 
       # REFUND THE CLUB BALANCE
-      refund_amount = club_transfer.amount
-      club = club_transfer.investment_club
-      new_balance = club.current_balance + refund_amount
-      club.update!(current_balance: new_balance)
+      # refund_amount = club_transfer.amount
+      # club = club_transfer.investment_club
+      # new_balance = club.current_balance + refund_amount
+      # club.update!(current_balance: new_balance)
 
-      Rails.logger.info "Club transfer #{transfer_code} failed. Refunded #{refund_amount} to club #{club.name}. New balance: #{new_balance}"
+      # Rails.logger.info "Club transfer #{transfer_code} failed. Refunded #{refund_amount} to club #{club.name}. New balance: #{new_balance}"
 
       # Send failure notification
       # send_club_transfer_failure_notification(club_transfer)
