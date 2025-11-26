41e71db4afa692416baa84a9d10c18c0f45ee242
josephadeabah <abeansah.lx@gmail.com>
2025-11-26 08:59:07 +0000

    fix: run all clocks at one min intervals

--- backend/app/controllers/api/v1/investment_clubs_controller.rb
+++ backend/app/controllers/api/v1/investment_clubs_controller.rb
@@ -237,7 +237,7 @@ module Api
           
           render json: {
             success: true,
-            club: ClubSerializer.new(club).as_json
+            club: InvestmentClubSerializer.new(club).as_json
           }
         else
           render json: {
