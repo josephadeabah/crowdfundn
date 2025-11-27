48be2593485e3d65f5245a137ea193abbbca99e6
josephadeabah <abeansah.lx@gmail.com>
2025-11-27 15:05:44 +0000

    fix: remove duplicate entries of bank

--- backend/app/models/transfer.rb
+++ backend/app/models/transfer.rb
@@ -6,11 +6,11 @@ class Transfer < ApplicationRecord
   validates :bank_name, :account_number, :amount, :transfer_code, :reference, presence: true
 
   # Add validation to ensure campaign exists if campaign_id is present
-  validate :campaign_exists, if: -> { campaign_id.present? }
+  # validate :campaign_exists, if: -> { campaign_id.present? }
 
-  private
+  # private
 
-  def campaign_exists
-    errors.add(:campaign, "does not exist") unless Campaign.exists?(campaign_id)
-  end
+  # def campaign_exists
+  #   errors.add(:campaign, "does not exist") unless Campaign.exists?(campaign_id)
+  # end
 end
