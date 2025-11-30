ddc24d5fdf88a1aa3a86ca45e2e8e5d17435b9f9
josephadeabah <abeansah.lx@gmail.com>
2025-11-30 23:10:31 +0000

    fix: homepage layout and design

--- frontend/app/components/home-navigation/Navigation.tsx
+++ frontend/app/components/home-navigation/Navigation.tsx
@@ -155,9 +155,9 @@ export default function Navigation({
               aria-label="Toggle menu"
             >
               {mobileMenuOpen ? (
-                <X className="w-5 h-5" />
+                <X className="w-5 h-5 text-gray-800" />
               ) : (
-                <Menu className="w-5 h-5" />
+                <Menu className="w-5 h-5 text-gray-800" />
               )}
             </button>
 
