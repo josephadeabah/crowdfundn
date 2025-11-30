7b3a8e66a87213510e836c5d8314542fd6d270f9
josephadeabah <abeansah.lx@gmail.com>
2025-11-30 23:12:15 +0000

    fix: homepage layout and design

--- frontend/app/components/home-navigation/Navigation.tsx
+++ frontend/app/components/home-navigation/Navigation.tsx
@@ -131,7 +131,7 @@ export default function Navigation({
                 variant={showAll ? 'default' : 'outline'}
                 size="sm"
                 className={cn(
-                  'gap-1.5 transition-all rounded-none text-xs',
+                  'gap-1.5 transition-all rounded-none text-xs bg-white',
                   showAll
                     ? 'bg-green-700/50 hover:bg-green-100/50 shadow-xs text-white'
                     : 'border-gray-50 text-gray-700 hover:bg-gray-50',
