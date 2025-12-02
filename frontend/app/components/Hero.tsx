f69babaa058af0b71ac0e3545fe92ec07825be57
josephadeabah <abeansah.lx@gmail.com>
2025-12-02 23:06:52 +0000

    fix: add more descriptive platform benefit marget one liner

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -210,7 +210,7 @@ const Hero = () => {
               {/* Main Heading */}
               <div className="space-y-4 pb-6">
                 <h1 className="text-3xl font-bold text-gray-700 leading-tight">
-                  Where SMEs Meet Investors:&nbsp;
+                  Where Startups & SMEs Meet Investors:&nbsp;
                   <span className="text-orange-500">
                     A Collaborative Platform For Raising Capital
                   </span>
