fbb6dff1f9d709fdfa381d96bcd404661e4f4ad4
josephadeabah <abeansah.lx@gmail.com>
2025-12-07 21:03:26 +0000

    fix: transferred amount update on campaigns

--- backend/app/models/equity_investment.rb
+++ backend/app/models/equity_investment.rb
@@ -491,7 +491,7 @@ class EquityInvestment < ApplicationRecord
   # Timestamp Methods
   def set_commitment_timestamps
     self.committed_at ||= Time.current
-    self.cancel_window_expires_at ||= 48.hours.from_now
+    self.cancel_window_expires_at ||= 1.minute.from_now
   end
 
   def set_investment_date
