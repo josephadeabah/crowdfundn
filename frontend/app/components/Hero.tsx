98a43d1bf4a41338cd147e57d9595cc83baa8d40
josephadeabah <abeansah.lx@gmail.com>
2025-12-25 01:25:27 +0000

    fix: hero message

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -222,7 +222,8 @@ const Hero = () => {
               {/* Main Heading */}
               <div className="space-y-4 pb-6">
                 <h1 className="text-3xl font-bold text-gray-700 leading-tight">
-                  Where Startups & SMEs Meet Investors:&nbsp;
+                  Where Startups, SMEs, Impact Projects & Change-Makers Meet
+                  Investors & Supporters:&nbsp;
                   <span className="text-orange-500">
                     A Collaborative Platform For Raising Capital
                   </span>
