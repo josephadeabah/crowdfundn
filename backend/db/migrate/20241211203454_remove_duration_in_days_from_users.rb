995c005074253df9b8a9dfe1822b4d73672895e9
josephadeabah <abeansah.lx@gmail.com>
2026-08-20 18:11:40 +0000

    fix: dration in days migration error

--- backend/db/migrate/20241211203454_remove_duration_in_days_from_users.rb
+++ backend/db/migrate/20241211203454_remove_duration_in_days_from_users.rb
@@ -1,5 +1,11 @@
+# db/migrate/20241211203454_remove_duration_in_days_from_users.rb
 class RemoveDurationInDaysFromUsers < ActiveRecord::Migration[7.1]
   def change
-    remove_column :users, :duration_in_days, :integer
+    # Only remove the column if it exists
+    if column_exists?(:users, :duration_in_days)
+      remove_column :users, :duration_in_days
+    else
+      puts "Column duration_in_days does not exist on users table. Skipping removal..."
+    end
   end
 end
