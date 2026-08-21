d9be9a43931e64586c1a36ffffaeeb9eb96a1c9b
josephadeabah <abeansah.lx@gmail.com>
2026-08-21 10:39:48 +0000

    fix: set puma to single mode

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -1,3 +1,4 @@
+# config/puma.rb
 # This configuration file will be evaluated by Puma.
 # The top-level methods that are invoked here are part of Puma's configuration DSL.
 
@@ -7,27 +8,20 @@ min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count).to_i
 
 threads min_threads_count, max_threads_count
 
-# Worker configuration for cluster mode
-if ENV['RAILS_ENV'] == 'production'
-  # Use 1 worker by default; override with WEB_CONCURRENCY
-  worker_count = ENV.fetch('WEB_CONCURRENCY', 1).to_i
+# Worker configuration - set to 0 for single mode
+worker_count = ENV.fetch('WEB_CONCURRENCY', 0).to_i
+workers worker_count
 
-  # Ensure at least 1 worker
-  worker_count = 1 if worker_count < 1
-
-  workers worker_count
-
-  # Preload application before forking workers
+# Only preload and configure workers if we have workers
+if worker_count > 0
   preload_app!
-
-  worker_timeout 60
-
+  
   fork_worker do
     if defined?(ActiveRecord::Base)
       ActiveRecord::Base.connection.disconnect!
     end
   end
-
+  
   on_worker_boot do
     if defined?(ActiveRecord::Base)
       ActiveRecord::Base.establish_connection
@@ -49,4 +43,3 @@ plugin :tmp_restart
 
 # For better performance with reverse proxies
 persistent_timeout 20
-wait_for_less_busy_worker 0.01
