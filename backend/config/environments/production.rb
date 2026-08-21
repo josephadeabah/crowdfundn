17c7432484506b8925c96272ef192c823d1eb414
josephadeabah <abeansah.lx@gmail.com>
2026-08-21 02:47:04 +0000

    fix: puma workers

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -6,8 +6,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
-
-  config.active_record.prepared_statements = false
+  
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
